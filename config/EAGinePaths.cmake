bd1afa449b4622ce6dd1bb5044ece7a4ce78dab5
Matus Chochlik <chochlik@gmail.com>
2021-06-25 07:09:52 +0200

    Additions to the build system + added gitignore
    - Added instructions to write INSTALL_PREFIX, BINARY_DIR and SOURCE_DIR files.

---
+++ config/EAGinePaths.cmake
@@ -0,0 +1,9 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#   http://www.boost.org/LICENSE_1_0.txt
+#
+file(WRITE "${PROJECT_BINARY_DIR}/SOURCE_DIR" "${PROJECT_SOURCE_DIR}")
+file(WRITE "${PROJECT_SOURCE_DIR}/BINARY_DIR" "${PROJECT_BINARY_DIR}")
+file(WRITE "${PROJECT_SOURCE_DIR}/INSTALL_PREFIX" "${CMAKE_INSTALL_PREFIX}")
+
