5e6286adc5cd191aed25cf58fdf8200695375bb2
Matus Chochlik <chochlik@gmail.com>
2021-07-14 07:55:00 +0200

    Moved doxygen and sphinx documentations configuration and sources from oglplu2

---
+++ doc/doxygen/include/core.hpp
@@ -0,0 +1,82 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+/// @mainpage The EAGine reference
+///
+/// This is the Doxygen-generated reference documentation for the EAGine project.
+///
+/// For the structured project documentation see the
+/// <a href="../sphinx/index.html">Sphinx-generated docs</a>.
+///
+/// @note This documentation is work-in-progress and incomplete.
+
+/// @defgroup build_config Build configuration settings
+
+/// @brief Common code is placed in this namespace.
+namespace eagine {
+
+/// @defgroup metaprogramming Metaprogramming utilities
+
+/// @defgroup type_utils Type utilities
+
+/// @defgroup memory Memory utilities
+
+/// @defgroup functional Functional and function-related utilities
+
+/// @defgroup string_utils String utilities
+
+/// @defgroup time_utils Time and duration utilities
+
+/// @defgroup identifiers Short string identifiers
+
+/// @defgroup logging Logging
+
+/// @defgroup valid_if Conditionally valid values
+
+/// @defgroup value_history Values and variables with history
+
+/// @defgroup serialization Serialization and deserialization
+
+/// @defgroup units Unit system
+
+/// @brief Math-related code is placed in this namespace.
+namespace math {
+/// @defgroup math Math utilities
+}
+
+/// @brief Value-tree code is placed in this namespace.
+namespace valtree {
+/// @defgroup valtree Value trees
+}
+
+/// @brief Message bus code is placed in this namespace.
+namespace msgbus {
+/// @defgroup msgbus Message bus
+}
+
+/// @brief Shape generator and loader code is placed in this namespace.
+namespace shapes {
+/// @defgroup shapes Shape generators and loaders
+}
+
+/// @defgroup main_context Program utilities and main context
+
+/// @defgroup c_api_wrap C-API wrappers
+
+/// @brief Application harness / wrapper code is placed in this namespace.
+namespace application {
+/// @defgroup application Application wrapper
+}
+
+/// @defgroup embedding Compiled-in resource blocks.
+
+/// @defgroup dyn_lib Dynamically-linked libraries
+
+/// @defgroup interop Interoperability
+
+} // namespace eagine
