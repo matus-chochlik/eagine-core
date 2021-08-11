14217ba7d00bde563e17ceefc5d870e5ae67cc8e
Matus Chochlik <chochlik@gmail.com>
2021-08-11 20:54:59 +0200

    Small tweaks in the from_string example

--- example/eagine/from_string.cpp
+++ example/eagine/from_string.cpp
@@ -19,18 +19,18 @@ auto main(main_ctx& ctx) -> int {
     using duration = std::chrono::duration<float, std::milli>;
 
     for(auto& arg : ctx.args()) {
-        if(auto opt_int = from_string<int>(arg)) {
+        if(const auto opt_int{from_string<int>(arg)}) {
             std::cout << "integer: " << extract(opt_int) << std::endl;
-        } else if(auto opt_float = from_string<float>(arg)) {
+        } else if(const auto opt_float{from_string<float>(arg)}) {
             std::cout << "float: " << extract(opt_float) << std::endl;
-        } else if(auto opt_double = from_string<double>(arg)) {
+        } else if(const auto opt_double{from_string<double>(arg)}) {
             std::cout << "double: " << extract(opt_double) << std::endl;
-        } else if(auto opt_char = from_string<char>(arg)) {
+        } else if(const auto opt_char{from_string<char>(arg)}) {
             std::cout << "character: " << extract(opt_char) << std::endl;
-        } else if(auto opt_bool = from_string<bool>(arg)) {
+        } else if(const auto opt_bool{from_string<bool>(arg)}) {
             std::cout << "boolean: " << std::boolalpha << extract(opt_bool)
                       << std::endl;
-        } else if(auto opt_dur = from_string<duration>(arg)) {
+        } else if(const auto opt_dur{from_string<duration>(arg)}) {
             std::cout << "duration: " << extract(opt_dur).count() << "ms"
                       << std::endl;
         } else {
