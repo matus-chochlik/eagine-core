6ac688dc26ad69389cb21f79f786c3a407078396
Matus Chochlik <chochlik@gmail.com>
2021-10-28 07:31:55 +0200

    Changes in the XML logs GUI viewer app
    - Tweaks in the connector images.
    - Moved the SVG sources of the connector images to a subdirectory.

--- source/app/xml_logs/images/render.sh
+++ source/app/xml_logs/images/render.sh
@@ -3,12 +3,12 @@
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at
 #  http://www.boost.org/LICENSE_1_0.txt
-for svg in $(dirname ${0})/*.svg
+for svg in $(dirname ${0})/src/*.svg
 do inkscape \
 	--pipe \
 	--export-area-page \
 	--export-overwrite \
 	--export-type=png \
-	--export-filename=${svg%.svg}.png \
+	--export-filename=$(dirname ${0})/$(basename ${svg} .svg).png \
 	< ${svg}
 done
