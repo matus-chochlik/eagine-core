6970a0b32c6fb7cc28eddcc9dbf6c102d7d55a21
Matus Chochlik <chochlik@gmail.com>
2022-03-11 07:25:37 +0100

    Made fixed_size_string constexpr

--- include/eagine/fixed_size_str.hpp
+++ include/eagine/fixed_size_str.hpp
@@ -13,7 +13,7 @@
 #include "int_constant.hpp"
 #include "string_span.hpp"
 #include "types.hpp"
-#include <cstring>
+#include <algorithm>
 
 namespace eagine {
 
@@ -24,8 +24,8 @@ class fixed_size_string {
 public:
     /// @brief Default construction.
     /// @post empty
-    fixed_size_string() noexcept {
-        std::memset(_str, '\0', N);
+    constexpr fixed_size_string() noexcept {
+        std::fill(std::begin(_str), std::end(_str), '\0');
     }
 
     /// @brief Construction from a pack of characters.
@@ -34,20 +34,19 @@ public:
       : _str{c...} {}
 
     /// @brief Construction from a C-string literal.
-    fixed_size_string(const char (&s)[N]) noexcept {
-        std::strncpy(_str, s, N);
-        EAGINE_ASSERT(_str[N - 1] == '\0');
+    constexpr fixed_size_string(const char (&s)[N]) noexcept {
+        std::copy(std::begin(s), std::end(s), std::begin(_str));
     }
 
     template <
       span_size_t N1,
       span_size_t N2,
       typename = std::enable_if_t<N1 + N2 == N + 1>>
-    fixed_size_string(
+    constexpr fixed_size_string(
       const fixed_size_string<N1>& s1,
       const fixed_size_string<N2>& s2) noexcept {
-        std::strncpy(_str, s1._str, N1);
-        std::strncpy(_str + N1 - 1, s2._str, N2);
+        std::copy(std::begin(s1._str), std::end(s1._str), _str);
+        std::copy(std::begin(s2._str), std::end(s1._str), _str + N1 - 1);
     }
 
     /// @brief Alias for length type.
@@ -60,56 +59,55 @@ public:
     using const_iterator = const char*;
 
     /// @brief Indicates if the string is empty.
-    auto empty() const noexcept {
+    constexpr auto empty() const noexcept {
         return _str[0] == '\0';
     }
 
     /// @brief Returns the size of the string.
-    auto size() const noexcept -> span_size_t {
+    constexpr auto size() const noexcept -> span_size_t {
         return N - 1;
     }
 
     /// @brief Returns pointer to the internally-stored character data.
-    auto data() const noexcept {
+    constexpr auto data() const noexcept {
         return static_cast<const char*>(_str);
     }
 
     /// @brief Returns pointer to the internally-stored character data.
     ///
     /// This function ensures that the internal storage is zero terminated.
-    auto c_str() const noexcept {
-        EAGINE_ASSERT(_str[N - 1] == '\0');
-        return data();
+    constexpr auto c_str() const noexcept {
+        return EAGINE_CONSTEXPR_ASSERT(_str[N - 1] == '\0', data());
     }
 
     /// @brief Returns iterator to the start of the string.
-    auto begin() noexcept -> iterator {
+    constexpr auto begin() noexcept -> iterator {
         return _str;
     }
 
     /// @brief Returns const iterator to the start of the string.
-    auto begin() const noexcept -> const_iterator {
+    constexpr auto begin() const noexcept -> const_iterator {
         return _str;
     }
 
     /// @brief Returns iterator past the end of the string.
-    auto end() noexcept -> iterator {
+    constexpr auto end() noexcept -> iterator {
         return _str + N - 1;
     }
 
     /// @brief Returns const iterator past the end of the string.
-    auto end() const noexcept -> const_iterator {
+    constexpr auto end() const noexcept -> const_iterator {
         return _str + N - 1;
     }
 
     /// @brief Returns a view over the internal character data storage.
-    auto view() const noexcept -> string_view {
+    constexpr auto view() const noexcept -> string_view {
         return {_str, N - 1};
     }
 
     /// @brief Returns a view over the internal character data storage.
     /// @see view
-    operator string_view() const noexcept {
+    constexpr operator string_view() const noexcept {
         return view();
     }
 
@@ -125,14 +123,14 @@ private:
 /// @brief Creates a fixed_size_string from a C-string literal.
 /// @ingroup string_utils
 template <span_size_t N>
-static inline auto make_fixed_size_string(const char (&str)[N]) noexcept {
+static constexpr auto make_fixed_size_string(const char (&str)[N]) noexcept {
     return fixed_size_string<N>(str);
 }
 
 /// @brief Concatenation operator for fixed_size_string.
 /// @ingroup string_utils
 template <span_size_t N1, span_size_t N2>
-static inline auto operator+(
+static constexpr auto operator+(
   const fixed_size_string<N1>& s1,
   const fixed_size_string<N2>& s2) noexcept {
     return fixed_size_string<N1 + N2 - 1>(s1, s2);
@@ -141,7 +139,7 @@ static inline auto operator+(
 /// @brief Converts a single-digit decimal number into fixed_size_string.
 /// @ingroup string_utils
 template <int I>
-static inline auto to_fixed_size_string(
+static constexpr auto to_fixed_size_string(
   const int_constant<I>,
   std::enable_if_t<(I >= 0) && (I < 10)>* = nullptr) noexcept {
     return fixed_size_string<2>(char('0' + I), '\0');
@@ -150,7 +148,7 @@ static inline auto to_fixed_size_string(
 /// @brief Converts a multi-digit decimal number into fixed_size_string.
 /// @ingroup string_utils
 template <int I>
-static inline auto to_fixed_size_string(
+static constexpr auto to_fixed_size_string(
   const int_constant<I>,
   std::enable_if_t<(I > 9)>* = nullptr) noexcept {
     return to_fixed_size_string(int_constant<I / 10>()) +
@@ -160,7 +158,7 @@ static inline auto to_fixed_size_string(
 /// @brief Converts a negative decimal number into fixed_size_string.
 /// @ingroup string_utils
 template <int I>
-static inline auto to_fixed_size_string(
+static constexpr auto to_fixed_size_string(
   const int_constant<I>,
   std::enable_if_t<(I < 0)>* = nullptr) noexcept {
     return fixed_size_string<2>("-") + to_fixed_size_string(int_constant<-I>());
