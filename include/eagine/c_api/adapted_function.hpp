e23d3f748ca8a157097e8383ce541b0d9bdf4a4c
Matus Chochlik <chochlik@gmail.com>
2022-03-16 07:35:46 +0100

    Changes in the C-API adapted_function + added combined_function

--- include/eagine/c_api/adapted_function.hpp
+++ include/eagine/c_api/adapted_function.hpp
@@ -36,30 +36,20 @@ template <auto ptr>
 using method_signature_t = typename method_traits<decltype(ptr)>::signature;
 
 template <
-  typename Api,
   typename Wrapper,
-  Wrapper Api::*method,
   typename CSignature = typename Wrapper::signature,
   typename CppSignature = CSignature,
   typename Map = trivial_map>
 class basic_adapted_function;
 
 template <
-  typename Api,
   typename Wrapper,
-  Wrapper Api::*method,
   typename CRV,
   typename... CParam,
   typename CppRV,
   typename... CppParam,
   typename Map>
-class basic_adapted_function<
-  Api,
-  Wrapper,
-  method,
-  CRV(CParam...),
-  CppRV(CppParam...),
-  Map> {
+class basic_adapted_function<Wrapper, CRV(CParam...), CppRV(CppParam...), Map> {
     template <std::size_t... I>
     constexpr auto _call(CppParam... param, std::index_sequence<I...>)
       const noexcept {
@@ -67,18 +57,18 @@ class basic_adapted_function<
         const Map map{};
         return map(
           size_constant<0>{},
-          Ftw::call(_api.*method, map(size_constant<I + 1>{}, 0, param...)...),
+          Ftw::call(_func, map(size_constant<I + 1>{}, 0, param...)...),
           param...);
     }
 
-    Api& _api;
+    Wrapper& _func;
 
 public:
-    basic_adapted_function(Api& api) noexcept
-      : _api{api} {}
+    basic_adapted_function(Wrapper& func) noexcept
+      : _func{func} {}
 
     explicit constexpr operator bool() const noexcept {
-        return bool(_api.*method);
+        return bool(_func);
     }
 
     constexpr auto operator()(CppParam... param) const noexcept {
@@ -91,13 +81,50 @@ template <
   auto method,
   typename CppSignature = method_signature_t<method>,
   typename Map = trivial_map>
-using adapted_function = basic_adapted_function<
-  method_api_t<method>,
-  method_wrapper_t<method>,
-  method,
-  method_signature_t<method>,
-  CppSignature,
-  Map>;
+struct adapted_function
+  : basic_adapted_function<
+      method_wrapper_t<method>,
+      method_signature_t<method>,
+      CppSignature,
+      Map> {
+    adapted_function(method_api_t<method>& api)
+      : basic_adapted_function<
+          method_wrapper_t<method>,
+          method_signature_t<method>,
+          CppSignature,
+          Map>{api.*method} {}
+};
+
+template <
+  typename ApiTraits,
+  typename Tag,
+  typename CSignature,
+  typename CppSignature,
+  typename Map,
+  function_ptr<ApiTraits, Tag, CSignature> function,
+  bool isAvailable,
+  bool isStatic>
+class combined_function {
+public:
+    template <typename Api>
+    combined_function(string_view name, ApiTraits& api_traits, Api& api)
+      : _basic{name, api_traits, api}
+      , _adapted{_basic} {}
+
+    constexpr explicit operator bool() const noexcept {
+        return bool(_basic);
+    }
+
+private:
+    opt_function<ApiTraits, Tag, CSignature, function, isAvailable, isStatic>
+      _basic;
+    basic_adapted_function<
+      opt_function<ApiTraits, Tag, CSignature, function, isAvailable, isStatic>,
+      CSignature,
+      CppSignature,
+      Map>
+      _adapted;
+};
 
 } // namespace eagine::c_api
 
