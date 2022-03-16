95711b2fcec7b7c736b83de1a8b381092e4be334
Matus Chochlik <chochlik@gmail.com>
2022-03-16 07:19:16 +0100

    Moved C-API default_traits into a separate header

--- include/eagine/c_api/function.hpp
+++ include/eagine/c_api/function.hpp
@@ -9,9 +9,7 @@
 #ifndef EAGINE_C_API_FUNCTION_HPP
 #define EAGINE_C_API_FUNCTION_HPP
 
-#include "../string_span.hpp"
-#include "../valid_if/always.hpp"
-#include "../valid_if/never.hpp"
+#include "api_traits.hpp"
 #include "result.hpp"
 
 namespace eagine::c_api {
@@ -51,69 +49,6 @@ class dynamic_function;
 //------------------------------------------------------------------------------
 template <typename Api, typename ApiTraits, typename Tag>
 class derived_function;
-//------------------------------------------------------------------------------
-/// @brief Policy class customizing C-API constant value loading and function linking.
-/// @ingroup c_api_wrap
-struct default_traits {
-    template <typename Tag, typename Signature>
-    using function_pointer = std::add_pointer<Signature>;
-
-    template <typename Api, typename Type>
-    auto load_constant(Api&, const string_view, const type_identity<Type>)
-      -> std::tuple<Type, bool> {
-        return {{}, false};
-    }
-
-    template <typename Api, typename Tag, typename Signature>
-    auto link_function(
-      Api&,
-      const Tag,
-      const string_view,
-      const type_identity<Signature>) -> std::add_pointer_t<Signature> {
-        return nullptr;
-    }
-
-    template <typename Tag, typename RV>
-    static constexpr auto fallback(const Tag, const type_identity<RV>) -> RV {
-        return {};
-    }
-
-    template <typename Tag>
-    static constexpr void fallback(const Tag, const type_identity<void>) {}
-
-    template <typename RV, typename Tag, typename... Params, typename... Args>
-    static constexpr auto call_static(
-      const Tag tag,
-      RV (*function)(Params...),
-      Args&&... args) -> RV {
-        if(function) {
-            return function(
-              std::forward<Args>(args)...); // NOLINT(hicpp-no-array-decay)
-        }
-        return fallback(tag, type_identity<RV>());
-    }
-
-    template <typename RV, typename Tag, typename... Params, typename... Args>
-    static constexpr auto call_dynamic(
-      const Tag tag,
-      RV (*function)(Params...),
-      Args&&... args) -> RV {
-        if(function) {
-            return function(
-              std::forward<Args>(args)...); // NOLINT(hicpp-no-array-decay)
-        }
-        return fallback(tag, type_identity<RV>());
-    }
-
-    template <typename RV>
-    using no_result = never_valid<RV>;
-
-    template <typename RV>
-    using opt_result = optionally_valid<RV>;
-
-    template <typename RV>
-    using result = always_valid<RV>;
-};
 //------------------------------------------------------------------------------
 template <bool isAvailable>
 class function_base : public bool_constant<isAvailable> {
