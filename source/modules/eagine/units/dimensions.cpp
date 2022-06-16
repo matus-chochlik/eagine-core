4adbcf112e55a08d4df07b03a0a5fbb0e790baaa
Matus Chochlik <chochlik@gmail.com>
2022-06-16 19:29:29 +0200

    Added new modularized code

--- source/modules/eagine/units/dimensions.cpp
+++ source/modules/eagine/units/dimensions.cpp
@@ -39,6 +39,13 @@ struct name_of<volume> {
     static constexpr const char mp_str[] = "volume";
 };
 //------------------------------------------------------------------------------
+export using mass_density = decltype(mass() / volume());
+
+export template <>
+struct name_of<mass_density> {
+    static constexpr const char mp_str[] = "mass density";
+};
+//------------------------------------------------------------------------------
 export using angular_velocity = decltype(angle() / time());
 
 export template <>
@@ -88,5 +95,70 @@ struct name_of<power> {
     static constexpr const char mp_str[] = "power";
 };
 //------------------------------------------------------------------------------
+export using pressure = decltype(force() / area());
+
+export template <>
+struct name_of<pressure> {
+    static constexpr const char mp_str[] = "pressure";
+};
+//------------------------------------------------------------------------------
+export using electric_charge = decltype(electric_current() * time());
+
+export template <>
+struct name_of<electric_charge> {
+    static constexpr const char mp_str[] = "electric charge";
+};
+//------------------------------------------------------------------------------
+export using electric_tension = decltype(power() / electric_current());
+
+export template <>
+struct name_of<electric_tension> {
+    static constexpr const char mp_str[] = "electric tension";
+};
+//------------------------------------------------------------------------------
+export using electrical_capacitance =
+  decltype(electric_charge() / electric_tension());
+
+export template <>
+struct name_of<electrical_capacitance> {
+    static constexpr const char mp_str[] = "electrical capacitance";
+};
+//------------------------------------------------------------------------------
+export using electrical_conductance =
+  decltype(electric_current() / electric_tension());
+
+export template <>
+struct name_of<electrical_conductance> {
+    static constexpr const char mp_str[] = "electrical conductance";
+};
+//------------------------------------------------------------------------------
+export using electrical_resistance =
+  decltype(electric_tension() / electric_current());
+
+export template <>
+struct name_of<electrical_resistance> {
+    static constexpr const char mp_str[] = "electrical resistance";
+};
+//------------------------------------------------------------------------------
+export using magnetic_flux = decltype(energy() / electric_current());
+
+export template <>
+struct name_of<magnetic_flux> {
+    static constexpr const char mp_str[] = "magnetic flux";
+};
+//------------------------------------------------------------------------------
+export using magnetic_field_strength = decltype(magnetic_flux() / area());
+
+export template <>
+struct name_of<magnetic_field_strength> {
+    static constexpr const char mp_str[] = "magnetic field strength";
+};
+//------------------------------------------------------------------------------
+export using inductance = decltype(magnetic_flux() / electric_current());
+
+export template <>
+struct name_of<inductance> {
+    static constexpr const char mp_str[] = "inductance";
+};
 //------------------------------------------------------------------------------
 } // namespace eagine::units
--- source/modules/eagine/units/si.cpp
+++ source/modules/eagine/units/si.cpp
@@ -50,6 +50,8 @@ export using quarter =
 export using turn =
   scaled_unit<scales::multiplied<scales::constant<2>, scales::pi>, radian>;
 
+export struct steradian : unit<solid_angle, steradian> {};
+
 // kelvin
 export struct kelvin : unit<temperature, kelvin> {};
 
@@ -65,9 +67,24 @@ export using hour = scaled_unit<scales::constant<3600>, second>;
 // day
 export using day = scaled_unit<scales::constant<86400>, second>;
 
+export struct mole : unit<amount_of_substance, mole> {};
+
+export struct count_of_cycles : unit<number_of_cycles, count_of_cycles> {};
+
+export struct count_of_decays : unit<number_of_decays, count_of_decays> {};
+
+export struct meter : unit<length, meter> {};
+
+export struct gram : unit<mass, gram> {};
+
+export using tonne = scaled_unit<scales::mega, gram>;
+
+export struct candela : unit<luminous_intensity, candela> {};
+
+export struct ampere : unit<electric_current, ampere> {};
+
 } // namespace base
 //------------------------------------------------------------------------------
-// units
 export using second = unit<time, si>;
 
 export using radian = unit<angle, si>;
@@ -135,6 +152,21 @@ export template <>
 struct si::base_unit<base::angle> : base::radian {};
 //------------------------------------------------------------------------------
 export template <>
+struct name_of<base::steradian> {
+    static constexpr const char mp_str[] = "steradian";
+};
+
+export template <>
+struct symbol_of<base::steradian> {
+    static constexpr const char mp_str[] = "sr";
+};
+
+export template <>
+struct si::base_unit<base::solid_angle> : base::steradian {};
+
+export using steradian = unit<solid_angle, si>;
+//------------------------------------------------------------------------------
+export template <>
 struct name_of<base::kelvin> {
     static constexpr const char mp_str[] = "kelvin";
 };
@@ -186,6 +218,324 @@ export struct fahrenheit_conv {
 };
 
 export using degree_fahrenheit = make_custom_unit_t<fahrenheit_conv, kelvin>;
+//------------------------------------------------------------------------------
+export template <>
+struct name_of<base::mole> {
+    static constexpr const char mp_str[] = "mole";
+};
+export template <>
+struct symbol_of<base::mole> {
+    static constexpr const char mp_str[] = "mol";
+};
+
+export template <>
+struct si::base_unit<base::amount_of_substance> : base::mole {};
+
+export using mole = unit<amount_of_substance, si>;
+
+export using kilomole =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::kilo, base::mole>, si>;
+//------------------------------------------------------------------------------
+export template <>
+struct si::base_unit<base::number_of_cycles> : base::count_of_cycles {};
+
+export using hertz = unit<frequency, si>;
+
+export template <>
+struct name_of<hertz> {
+    static constexpr const char mp_str[] = "hertz";
+};
+
+export template <>
+struct symbol_of<hertz> {
+    static constexpr const char mp_str[] = "Hz";
+};
+
+export using kilohertz = make_scaled_unit_t<scales::kilo, hertz>;
+export using megahertz = make_scaled_unit_t<scales::mega, hertz>;
+export using gigahertz = make_scaled_unit_t<scales::giga, hertz>;
+//------------------------------------------------------------------------------
+export template <>
+struct si::base_unit<base::number_of_decays> : base::count_of_decays {};
+
+export using count_of_decays = unit<number_of_decays, si>;
+
+export using becquerel = unit<radioactivity, si>;
+
+export template <>
+struct name_of<becquerel> {
+    static constexpr const char mp_str[] = "becquerel";
+};
+
+export template <>
+struct symbol_of<becquerel> {
+    static constexpr const char mp_str[] = "Bq";
+};
+
+export using kilobecquerel = make_scaled_unit_t<scales::kilo, becquerel>;
+export using megabecquerel = make_scaled_unit_t<scales::mega, becquerel>;
+export using gigabecquerel = make_scaled_unit_t<scales::giga, becquerel>;
+//------------------------------------------------------------------------------
+export template <>
+struct name_of<base::meter> {
+    static constexpr const char mp_str[] = "meter";
+};
+export template <>
+struct symbol_of<base::meter> {
+    static constexpr const char mp_str[] = "m";
+};
+
+export template <>
+struct si::base_unit<base::length> : base::meter {};
+
+export using meter = unit<length, si>;
+
+export using kilometer =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::kilo, base::meter>, si>;
+
+export using decimeter =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::deci, base::meter>, si>;
+
+export using centimeter =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::centi, base::meter>, si>;
+
+export using millimeter =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::milli, base::meter>, si>;
+//------------------------------------------------------------------------------
+export template <>
+struct name_of<base::gram> {
+    static constexpr const char mp_str[] = "gram";
+};
+export template <>
+struct symbol_of<base::gram> {
+    static constexpr const char mp_str[] = "g";
+};
+
+export template <>
+struct name_of<base::tonne> {
+    static constexpr const char mp_str[] = "tonne";
+};
+
+export template <>
+struct symbol_of<base::tonne> {
+    static constexpr const char mp_str[] = "t";
+};
+
+export template <>
+struct si::base_unit<base::mass>
+  : base::scaled_unit<scales::kilo, base::gram> {};
+
+export using kilogram = unit<mass, si>;
+
+export using gram =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::one, base::gram>, si>;
+
+export using milligram =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::milli, base::gram>, si>;
+
+export using tonne = make_scaled_base_dim_unit_t<base::tonne, si>;
+//------------------------------------------------------------------------------
+export template <>
+struct name_of<base::candela> {
+    static constexpr const char mp_str[] = "candela";
+};
+
+export template <>
+struct symbol_of<base::candela> {
+    static constexpr const char mp_str[] = "cd";
+};
+
+export template <>
+struct si::base_unit<base::luminous_intensity> : base::candela {};
+
+export using candela = unit<luminous_intensity, si>;
+
+export using kilocandela =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::kilo, base::candela>, si>;
+//------------------------------------------------------------------------------
+export using newton = unit<force, si>;
+
+export template <>
+struct name_of<newton> {
+    static constexpr const char mp_str[] = "newton";
+};
+
+export template <>
+struct symbol_of<newton> {
+    static constexpr const char mp_str[] = "N";
+};
+
+export using kilonewton = make_scaled_unit_t<scales::kilo, newton>;
+export using meganewton = make_scaled_unit_t<scales::mega, newton>;
+//------------------------------------------------------------------------------
+export using joule = unit<energy, si>;
+
+export template <>
+struct name_of<joule> {
+    static constexpr const char mp_str[] = "joule";
+};
+
+export template <>
+struct symbol_of<joule> {
+    static constexpr const char mp_str[] = "J";
+};
+
+export using kilojoule = make_scaled_unit_t<scales::kilo, joule>;
+export using megajoule = make_scaled_unit_t<scales::mega, joule>;
+//------------------------------------------------------------------------------
+export using watt = unit<power, si>;
+
+export template <>
+struct name_of<watt> {
+    static constexpr const char mp_str[] = "watt";
+};
+
+export template <>
+struct symbol_of<watt> {
+    static constexpr const char mp_str[] = "W";
+};
+
+export using kilowatt = make_scaled_unit_t<scales::kilo, watt>;
+export using megawatt = make_scaled_unit_t<scales::mega, watt>;
+//------------------------------------------------------------------------------
+export using pascal = unit<pressure, si>;
+
+export template <>
+struct name_of<pascal> {
+    static constexpr const char mp_str[] = "pascal";
+};
+
+export template <>
+struct symbol_of<pascal> {
+    static constexpr const char mp_str[] = "Pa";
+};
+
+export using kilopascal = make_scaled_unit_t<scales::kilo, pascal>;
+export using megapascal = make_scaled_unit_t<scales::mega, pascal>;
+//------------------------------------------------------------------------------
+export template <>
+struct name_of<base::ampere> {
+    static constexpr const char mp_str[] = "ampere";
+};
+export template <>
+struct symbol_of<base::ampere> {
+    static constexpr const char mp_str[] = "A";
+};
+
+export template <>
+struct si::base_unit<base::electric_current> : base::ampere {};
+
+export using ampere = unit<electric_current, si>;
+
+export using kiloampere =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::kilo, base::ampere>, si>;
+
+export using milliampere =
+  make_scaled_base_dim_unit_t<base::scaled_unit<scales::milli, base::ampere>, si>;
+//------------------------------------------------------------------------------
+export using coulomb = unit<electric_charge, si>;
+
+export template <>
+struct name_of<coulomb> {
+    static constexpr const char mp_str[] = "coulomb";
+};
+
+export template <>
+struct symbol_of<coulomb> {
+    static constexpr const char mp_str[] = "C";
+};
+
+export using millicoulomb = make_scaled_unit_t<scales::milli, coulomb>;
+export using kilocoulomb = make_scaled_unit_t<scales::kilo, coulomb>;
+//------------------------------------------------------------------------------
+export using volt = unit<electric_tension, si>;
+
+export template <>
+struct name_of<volt> {
+    static constexpr const char mp_str[] = "volt";
+};
+
+export template <>
+struct symbol_of<volt> {
+    static constexpr const char mp_str[] = "V";
+};
+
+export using millivolt = make_scaled_unit_t<scales::milli, volt>;
+export using kilovolt = make_scaled_unit_t<scales::kilo, volt>;
+//------------------------------------------------------------------------------
+export using farad = unit<electrical_capacitance, si>;
+
+export template <>
+struct name_of<farad> {
+    static constexpr const char mp_str[] = "farad";
+};
+
+export template <>
+struct symbol_of<farad> {
+    static constexpr const char mp_str[] = "F";
+};
+
+export using microfarad = make_scaled_unit_t<scales::micro, farad>;
+export using millifarad = make_scaled_unit_t<scales::milli, farad>;
+export using kilofarad = make_scaled_unit_t<scales::kilo, farad>;
+//------------------------------------------------------------------------------
+export using siemens = unit<electrical_conductance, si>;
+
+export template <>
+struct name_of<siemens> {
+    static constexpr const char mp_str[] = "siemens";
+};
+
+export template <>
+struct symbol_of<siemens> {
+    static constexpr const char mp_str[] = "S";
+};
+
+export using millisiemens = make_scaled_unit_t<scales::milli, siemens>;
+//------------------------------------------------------------------------------
+export using ohm = unit<electrical_resistance, si>;
+
+export template <>
+struct name_of<ohm> {
+    static constexpr const char mp_str[] = "ohm";
+};
+
+export template <>
+struct symbol_of<ohm> {
+    static constexpr const char mp_str[] = {char(0xCE), char(0xA9), '\0'};
+};
+
+export using milliohm = make_scaled_unit_t<scales::milli, ohm>;
+export using kiloohm = make_scaled_unit_t<scales::kilo, ohm>;
+//------------------------------------------------------------------------------
+export using weber = unit<magnetic_flux, si>;
+
+export template <>
+struct name_of<weber> {
+    static constexpr const char mp_str[] = "weber";
+};
+
+export template <>
+struct symbol_of<weber> {
+    static constexpr const char mp_str[] = "Wb";
+};
+
+export using milliweber = make_scaled_unit_t<scales::milli, weber>;
+export using kiloweber = make_scaled_unit_t<scales::kilo, weber>;
+//------------------------------------------------------------------------------
+export using tesla = unit<magnetic_field_strength, si>;
+
+export template <>
+struct name_of<tesla> {
+    static constexpr const char mp_str[] = "tesla";
+};
+
+export template <>
+struct symbol_of<tesla> {
+    static constexpr const char mp_str[] = "T";
+};
+
+export using millitesla = make_scaled_unit_t<scales::milli, tesla>;
 //------------------------------------------------------------------------------
 } // namespace units
 } // namespace eagine
