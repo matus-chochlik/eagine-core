252e04ed65a32fc4bfe6ad122510ded21a2a9750
Matus Chochlik <chochlik@gmail.com>
2022-11-09 13:20:34 +0100

    Added puzzle_progress

--- source/modules/eagine/progress/activity.cpp
+++ source/modules/eagine/progress/activity.cpp
@@ -17,6 +17,7 @@ namespace eagine {
 //------------------------------------------------------------------------------
 /// @brief Class tracking the progress of a long-running activity.
 /// @ingroup progress
+/// @see puzzle_progress
 export class activity_progress {
 public:
     activity_progress(
@@ -113,6 +114,39 @@ protected:
 private:
     std::shared_ptr<progress_tracker_backend> _backend;
     activity_progress_id_t _activity_id{0U};
+};
+//------------------------------------------------------------------------------
+/// @brief Class tracking the completion of Count objects, indicating true/false status.
+/// @ingroup progress
+/// @see activity_progress
+export template <span_size_t Count>
+class puzzle_progress : activity_progress {
+public:
+    /// @brief Construction from a parent activity and this activity title.
+    puzzle_progress(
+      const activity_progress& parent,
+      const string_view title) noexcept
+      : activity_progress{parent, title, Count} {}
+
+    /// @brief Indicates that all the pieces indicates true state.
+    auto done() const noexcept -> bool {
+        return _all_done;
+    }
+
+    /// @brief Updates the progress value by counting pieces returning true.
+    template <typename... Piece>
+    auto update_progress(const Piece&... piece) noexcept -> bool {
+        const bool result{
+          activity_progress::update_progress((... + span_size_t(bool(piece))))};
+        _all_done = (bool(piece) && ...);
+        if(_all_done) {
+            activity_progress::finish();
+        }
+        return result;
+    }
+
+private:
+    bool _all_done{false};
 };
 //------------------------------------------------------------------------------
 } // namespace eagine
