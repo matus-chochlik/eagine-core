4cf24627aff93b6157c4e75f89e7e6b125804941
Matus Chochlik <chochlik@gmail.com>
2022-11-21 08:58:31 +0100

    Updated example (sudoku_tiling)

--- example/eagine/sudoku_tiling.cpp
+++ example/eagine/sudoku_tiling.cpp
@@ -11,18 +11,22 @@ import <stack>;
 import <map>;
 
 template <unsigned S>
-void sudoku_tiling() {
+void sudoku_tiling(eagine::span_size_t w, eagine::span_size_t h) {
     using namespace eagine;
     const basic_sudoku_board_traits<S> traits;
 
-    basic_sudoku_tile_patch<S> patch(32, 24);
+    basic_sudoku_tile_patch<S> patch(w, h);
     basic_sudoku_tiling<S> bst{
       traits, traits.make_generator().generate_medium()};
     std::cout << bst.fill(0, 0, patch);
 }
 
-auto main() -> int {
-    sudoku_tiling<4>();
+auto main(int argc, const char** argv) -> int {
+    eagine::program_args args{argc, argv};
+
+    sudoku_tiling<4>(
+      args.find_value_or("--width", 32, std::cerr),
+      args.find_value_or("--height", 24, std::cerr));
 
     return 0;
 }
