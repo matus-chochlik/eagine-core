dc358e0dac4d9685e71b263cc8ad0404d0316a3c
Matus Chochlik <chochlik@gmail.com>
2022-11-30 21:12:11 +0100

    Added tests for vect::diff

---
+++ source/modules/eagine/vectorization/diff_test.cpp
@@ -0,0 +1,70 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <eagine/testing/unit_begin.hpp>
+import eagine.core.vectorization;
+//------------------------------------------------------------------------------
+template <typename T, int N, bool V>
+void vect_diff_TNV_1(eagitest::case_& test) {
+    auto& rg{test.random()};
+
+    for(unsigned k = 0; k < test.repeats(1000); ++k) {
+        T a[N];
+
+        typename eagine::vect::data<T, N, V>::type u = {}, v = {};
+
+        for(int i = 0; i < N; ++i) {
+            a[i] = rg.get_between<T>(-10000, +10000);
+            u[i] = a[i];
+            v[i] = a[i];
+        }
+
+        using _diff = eagine::vect::diff<T, N, V>;
+        using _esum = eagine::vect::esum<T, N, V>;
+
+        test.check_equal(_esum::apply(_diff::apply(u, u)), T(0), "u=u");
+        test.check_equal(_esum::apply(_diff::apply(v, u)), T(0), "v=u");
+        test.check_equal(_esum::apply(_diff::apply(u, v)), T(0), "u=v");
+        test.check_equal(_esum::apply(_diff::apply(v, v)), T(0), "v=v");
+    }
+}
+//------------------------------------------------------------------------------
+template <typename T, bool V>
+void vect_diff_TV_1(eagitest::case_& test) {
+    vect_diff_TNV_1<T, 2, V>(test);
+    vect_diff_TNV_1<T, 3, V>(test);
+    vect_diff_TNV_1<T, 4, V>(test);
+    vect_diff_TNV_1<T, 5, V>(test);
+    vect_diff_TNV_1<T, 7, V>(test);
+    vect_diff_TNV_1<T, 8, V>(test);
+    vect_diff_TNV_1<T, 11, V>(test);
+    vect_diff_TNV_1<T, 15, V>(test);
+    vect_diff_TNV_1<T, 19, V>(test);
+}
+//------------------------------------------------------------------------------
+template <typename T>
+void vect_diff_T_1(eagitest::case_& test) {
+    vect_diff_TV_1<T, true>(test);
+    vect_diff_TV_1<T, false>(test);
+}
+//------------------------------------------------------------------------------
+void vect_diff_1(auto& s) {
+    eagitest::case_ test{s, 1, "1"};
+
+    vect_diff_T_1<int>(test);
+    vect_diff_T_1<float>(test);
+    vect_diff_T_1<double>(test);
+}
+//------------------------------------------------------------------------------
+auto main(int argc, const char** argv) -> int {
+    eagitest::suite test{argc, argv, "vect_diff", 1};
+    test.once(vect_diff_1);
+    return test.exit_code();
+}
+//------------------------------------------------------------------------------
+#include <eagine/testing/unit_end.hpp>
