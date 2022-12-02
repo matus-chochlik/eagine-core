5f193d8a9fc0aff21dfd9cb4365008d1ec19705c
Matus Chochlik <chochlik@gmail.com>
2022-12-02 09:00:52 +0100

    Added tests for bitfield

--- source/modules/eagine/types/bitfield_test.cpp
+++ source/modules/eagine/types/bitfield_test.cpp
@@ -37,6 +37,8 @@ auto test_bit_list() noexcept -> std::array<std::tuple<unsigned, test_bit>, 8> {
        {7U, test_bit::bit_7}}};
 }
 //------------------------------------------------------------------------------
+// default construction
+//------------------------------------------------------------------------------
 void bitfield_default_construct(auto& s) {
     eagitest::case_ test{s, 1, "default construct"};
     eagitest::track trck{test, 0, 4};
@@ -103,6 +105,8 @@ void bitfield_default_construct(auto& s) {
       "!has_all");
 }
 //------------------------------------------------------------------------------
+// underlying bits
+//------------------------------------------------------------------------------
 void bitfield_underlying_bits_construct_impl(unsigned u, auto& s) {
     eagitest::case_ test{s, 2, "underlying construct"};
     eagitest::track trck{test, 0, 3};
@@ -158,10 +162,175 @@ void bitfield_underlying_bits_construct(unsigned run, auto& s) {
     }
 }
 //------------------------------------------------------------------------------
+// operators 1
+//------------------------------------------------------------------------------
+void bitfield_bit_operators_1(auto& s) {
+    eagitest::case_ test{s, 3, "operators 1"};
+    eagine::bitfield<test_bit> bf1(test_bit::bit_3);
+
+    test.check(bool(bf1), "is true");
+    test.check(!!bf1, "is not false");
+
+    test.check(!bf1.has(test_bit::bit_1), "has not 1 1");
+    test.check(!bf1.has(test_bit::bit_2), "has not 1 2");
+    test.check(bf1.has(test_bit::bit_3), "has 1 3");
+    test.check(!bf1.has(test_bit::bit_4), "has not 1 4");
+    test.check(!bf1.has(test_bit::bit_5), "has not 1 5");
+
+    eagine::bitfield<test_bit> bf2(test_bit::bit_1, test_bit::bit_5);
+
+    test.check(bf2.has(test_bit::bit_1), "has 2 1");
+    test.check(!bf2.has(test_bit::bit_2), "has not 2 2");
+    test.check(!bf2.has(test_bit::bit_3), "has not 2 3");
+    test.check(!bf2.has(test_bit::bit_4), "has not 2 4");
+    test.check(bf2.has(test_bit::bit_5), "has 2 5");
+
+    eagine::bitfield<test_bit> bf3 = bf2 | test_bit::bit_3;
+
+    test.check(bf3.has(test_bit::bit_1), "has 3 1");
+    test.check(!bf3.has(test_bit::bit_2), "has not 3 1");
+    test.check(bf3.has(test_bit::bit_3), "has 3 1");
+    test.check(!bf3.has(test_bit::bit_4), "has not 3 1");
+    test.check(bf3.has(test_bit::bit_5), "has 3 1");
+
+    test.check(!(bf1 == bf2), "not equal 1");
+    test.check(!(bf1 == bf3), "not equal 2");
+    test.check(bf1 != bf2, "not equal 3");
+    test.check(bf1 != bf3, "not equal 4");
+
+    eagine::bitfield<test_bit> bf4 = bf1 | bf2;
+
+    test.check(bf4.has(test_bit::bit_1), "has 4 1");
+    test.check(!bf4.has(test_bit::bit_2), "has not 4 2");
+    test.check(bf4.has(test_bit::bit_3), "has 4 3");
+    test.check(!bf4.has(test_bit::bit_4), "has not 4 4");
+    test.check(bf4.has(test_bit::bit_5), "has 4 5");
+
+    test.check(!bool(bf1 & bf2), "and is false");
+    test.check(!(bf1 & bf2), "and is not true");
+
+    eagine::bitfield<test_bit> bf5 = bf1 & bf2;
+
+    test.check(!bf5.has(test_bit::bit_1), "has not 5 1");
+    test.check(!bf5.has(test_bit::bit_2), "has not 5 2");
+    test.check(!bf5.has(test_bit::bit_3), "has not 5 3");
+    test.check(!bf5.has(test_bit::bit_4), "has not 5 4");
+    test.check(!bf5.has(test_bit::bit_5), "has not 5 5");
+
+    test.check(bool(bf1 & bf3), "and is true");
+    test.check(!!(bf1 & bf3), "and is not false");
+
+    eagine::bitfield<test_bit> bf6 = bf1 & bf3;
+
+    test.check(!bf6.has(test_bit::bit_1), "has not 6 1");
+    test.check(!bf6.has(test_bit::bit_2), "has not 6 2");
+    test.check(bf6.has(test_bit::bit_3), "has 6 3");
+    test.check(!bf6.has(test_bit::bit_4), "has not 6 4");
+    test.check(!bf6.has(test_bit::bit_5), "has 6 5");
+}
+//------------------------------------------------------------------------------
+// operators 2
+//------------------------------------------------------------------------------
+void bitfield_bit_operators_2(auto& s) {
+    eagitest::case_ test{s, 4, "operators 2"};
+
+    eagine::bitfield<test_bit> bf0;
+
+    test.check(!bf0.has(test_bit::bit_1), "has not 1 1");
+    test.check(!bf0.has(test_bit::bit_2), "has not 1 2");
+    test.check(!bf0.has(test_bit::bit_3), "has not 1 3");
+    test.check(!bf0.has(test_bit::bit_4), "has not 1 4");
+    test.check(!bf0.has(test_bit::bit_5), "has not 1 5");
+
+    bf0 |= test_bit::bit_3;
+
+    test.check(!bf0.has(test_bit::bit_1), "has not 2 1");
+    test.check(!bf0.has(test_bit::bit_2), "has not 2 2");
+    test.check(bf0.has(test_bit::bit_3), "has 2 3");
+    test.check(!bf0.has(test_bit::bit_4), "has not 2 4");
+    test.check(!bf0.has(test_bit::bit_5), "has not 2 5");
+
+    bf0 |= test_bit::bit_1;
+
+    test.check(bf0.has(test_bit::bit_1), "has 3 1");
+    test.check(!bf0.has(test_bit::bit_2), "has not 3 2");
+    test.check(bf0.has(test_bit::bit_3), "has 3 3");
+    test.check(!bf0.has(test_bit::bit_4), "has not 3 4");
+    test.check(!bf0.has(test_bit::bit_5), "has not 3 5");
+
+    bf0 |= test_bit::bit_5;
+
+    test.check(bf0.has(test_bit::bit_1), "has 4 1");
+    test.check(!bf0.has(test_bit::bit_2), "has not 4 2");
+    test.check(bf0.has(test_bit::bit_3), "has 4 3");
+    test.check(!bf0.has(test_bit::bit_4), "has not 4 4");
+    test.check(bf0.has(test_bit::bit_5), "has 4 5");
+
+    bf0 |= test_bit::bit_2;
+    bf0 |= test_bit::bit_4;
+
+    test.check(bf0.has(test_bit::bit_1), "has 5 1");
+    test.check(bf0.has(test_bit::bit_2), "has 5 2");
+    test.check(bf0.has(test_bit::bit_3), "has 5 3");
+    test.check(bf0.has(test_bit::bit_4), "has 5 4");
+    test.check(bf0.has(test_bit::bit_5), "has 5 5");
+
+    bf0 &= test_bit::bit_3;
+
+    test.check(!bf0.has(test_bit::bit_1), "has not 6 1");
+    test.check(!bf0.has(test_bit::bit_2), "has not 6 2");
+    test.check(bf0.has(test_bit::bit_3), "has 6 3");
+    test.check(!bf0.has(test_bit::bit_4), "has not 6 4");
+    test.check(!bf0.has(test_bit::bit_5), "has not 6 5");
+}
+//------------------------------------------------------------------------------
+// operators 3
+//------------------------------------------------------------------------------
+void bitfield_bit_operators_3(auto& s) {
+    eagitest::case_ test{s, 5, "operators 3"};
+
+    eagine::bitfield<test_bit> bf0(test_bit::bit_2, test_bit::bit_4);
+
+    test.check(!bf0.has(test_bit::bit_1), "has not 1 1");
+    test.check(bf0.has(test_bit::bit_2), "has 1 2");
+    test.check(!bf0.has(test_bit::bit_3), "has not 1 3");
+    test.check(bf0.has(test_bit::bit_4), "has 1 4");
+    test.check(!bf0.has(test_bit::bit_5), "has not 1 5");
+
+    eagine::bitfield<test_bit> bf1 = ~bf0;
+
+    test.check(bf1.has(test_bit::bit_1), "has 2 1");
+    test.check(!bf1.has(test_bit::bit_2), "has not 2 2");
+    test.check(bf1.has(test_bit::bit_3), "has 2 3");
+    test.check(!bf1.has(test_bit::bit_4), "has not 2 4");
+    test.check(bf1.has(test_bit::bit_5), "has 2 5");
+
+    eagine::bitfield<test_bit> bfo = bf0 | bf1;
+
+    test.check(bfo.has(test_bit::bit_1), "has 3 1");
+    test.check(bfo.has(test_bit::bit_2), "has 3 2");
+    test.check(bfo.has(test_bit::bit_3), "has 3 3");
+    test.check(bfo.has(test_bit::bit_4), "has 3 4");
+    test.check(bfo.has(test_bit::bit_5), "has 3 5");
+
+    eagine::bitfield<test_bit> bfa = bf0 & bf1;
+
+    test.check(!bfa.has(test_bit::bit_1), "has not 4 1");
+    test.check(!bfa.has(test_bit::bit_2), "has not 4 2");
+    test.check(!bfa.has(test_bit::bit_3), "has not 4 3");
+    test.check(!bfa.has(test_bit::bit_4), "has not 4 4");
+    test.check(!bfa.has(test_bit::bit_5), "has not 4 5");
+}
+//------------------------------------------------------------------------------
+// main
+//------------------------------------------------------------------------------
 auto main(int argc, const char** argv) -> int {
-    eagitest::suite test{argc, argv, "bitfield", 2};
+    eagitest::suite test{argc, argv, "bitfield", 5};
     test.once(bitfield_default_construct);
     test.repeat(100, bitfield_underlying_bits_construct);
+    test.once(bitfield_bit_operators_1);
+    test.once(bitfield_bit_operators_2);
+    test.once(bitfield_bit_operators_3);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
