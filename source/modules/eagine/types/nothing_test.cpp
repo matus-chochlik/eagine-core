77926262a7f03c08bd36d7ee1e7002ec98edd0d0
Matus Chochlik <chochlik@gmail.com>
2022-12-24 09:46:38 +0100

    Added some tests for nothing_t

---
+++ source/modules/eagine/types/nothing_test.cpp
@@ -0,0 +1,31 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <eagine/testing/unit_begin.hpp>
+import eagine.core.types;
+//------------------------------------------------------------------------------
+void nothing_default_construct(auto& s) {
+    eagitest::case_ test{s, 1, "default construct"};
+    eagine::nothing_t n;
+    test.constructed(n, "n");
+}
+//------------------------------------------------------------------------------
+void nothing_constant(auto& s) {
+    eagitest::case_ test{s, 2, "constant"};
+    eagine::nothing_t n{eagine::nothing};
+    test.constructed(n, "n");
+}
+//------------------------------------------------------------------------------
+auto main(int argc, const char** argv) -> int {
+    eagitest::suite test{argc, argv, "nothing", 2};
+    test.once(nothing_default_construct);
+    test.once(nothing_constant);
+    return test.exit_code();
+}
+//------------------------------------------------------------------------------
+#include <eagine/testing/unit_end.hpp>
