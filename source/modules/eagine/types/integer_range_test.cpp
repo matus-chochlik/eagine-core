6eb4578c2f99d096ea04a5da9c410118df4e42a2
Matus Chochlik <chochlik@gmail.com>
2022-12-24 10:20:04 +0100

    Added some tests for integer_range

---
+++ source/modules/eagine/types/integer_range_test.cpp
@@ -0,0 +1,27 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <eagine/testing/unit_begin.hpp>
+import eagine.core.types;
+//------------------------------------------------------------------------------
+void integer_range_1(auto& s) {
+    eagitest::case_ test{s, 1, "1"};
+    int i = 0;
+
+    for(const auto j : eagine::integer_range(10000)) {
+        test.check_equal(i++, j, "equal");
+    }
+}
+//------------------------------------------------------------------------------
+auto main(int argc, const char** argv) -> int {
+    eagitest::suite test{argc, argv, "integer_range", 1};
+    test.once(integer_range_1);
+    return test.exit_code();
+}
+//------------------------------------------------------------------------------
+#include <eagine/testing/unit_end.hpp>
