4b4720f7b553a013500618a2757f1b76b231d5e6
Matus Chochlik <chochlik@gmail.com>
2022-12-30 12:15:24 +0100

    Added some tests for units/dimensions

--- source/modules/eagine/units/dimensions_test.cpp
+++ source/modules/eagine/units/dimensions_test.cpp
@@ -109,16 +109,117 @@ void dimension_acceleration(auto& s) {
     });
 }
 //------------------------------------------------------------------------------
+// momentum
+//------------------------------------------------------------------------------
+void dimension_momentum(auto& s) {
+    eagitest::case_ test{s, 7, "momentum"};
+    eagine::units::base::for_each_dim([&](auto bd) {
+        test.check(
+          get_pow_of_dim(bd, eagine::units::momentum()) ==
+            get_pow_of_dim(bd, eagine::units::velocity()) +
+              get_pow_of_dim(bd, eagine::units::mass()),
+          "1");
+    });
+}
+//------------------------------------------------------------------------------
+// force
+//------------------------------------------------------------------------------
+void dimension_force(auto& s) {
+    eagitest::case_ test{s, 8, "force"};
+    eagine::units::base::for_each_dim([&](auto bd) {
+        test.check(
+          get_pow_of_dim(bd, eagine::units::force()) ==
+            get_pow_of_dim(bd, eagine::units::acceleration()) +
+              get_pow_of_dim(bd, eagine::units::mass()),
+          "1");
+
+        test.check(
+          get_pow_of_dim(bd, eagine::units::force()) ==
+            get_pow_of_dim(bd, eagine::units::velocity()) +
+              get_pow_of_dim(bd, eagine::units::mass()) -
+              get_pow_of_dim(bd, eagine::units::time()),
+          "2");
+    });
+}
+//------------------------------------------------------------------------------
+// energy
+//------------------------------------------------------------------------------
+void dimension_energy(auto& s) {
+    eagitest::case_ test{s, 9, "energy"};
+    eagine::units::base::for_each_dim([&](auto bd) {
+        test.check(
+          get_pow_of_dim(bd, eagine::units::energy()) ==
+            get_pow_of_dim(bd, eagine::units::force()) +
+              get_pow_of_dim(bd, eagine::units::length()),
+          "1");
+
+        test.check(
+          get_pow_of_dim(bd, eagine::units::energy()) ==
+            get_pow_of_dim(bd, eagine::units::length()) -
+              get_pow_of_dim(bd, eagine::units::time()) +
+              get_pow_of_dim(bd, eagine::units::length()) -
+              get_pow_of_dim(bd, eagine::units::time()) +
+              get_pow_of_dim(bd, eagine::units::mass()),
+          "2");
+    });
+}
+//------------------------------------------------------------------------------
+// power
+//------------------------------------------------------------------------------
+void dimension_power(auto& s) {
+    eagitest::case_ test{s, 10, "power"};
+    eagine::units::base::for_each_dim([&](auto bd) {
+        test.check(
+          get_pow_of_dim(BD(), eagine::units::power()) ==
+            get_pow_of_dim(BD(), eagine::units::energy()) -
+              get_pow_of_dim(BD(), eagine::units::time()),
+          "1");
+
+        test.check(
+          get_pow_of_dim(BD(), eagine::units::power()) ==
+            get_pow_of_dim(BD(), eagine::units::area()) +
+              get_pow_of_dim(BD(), eagine::units::mass()) -
+              get_pow_of_dim(BD(), eagine::units::time()) * 3,
+          "2");
+    });
+}
+//------------------------------------------------------------------------------
+// pressure
+//------------------------------------------------------------------------------
+void dimension_pressure(auto& s) {
+    eagitest::case_ test{s, 11, "pressure"};
+    eagine::units::base::for_each_dim([&](auto bd) {
+        test.check(
+          get_pow_of_dim(BD(), eagine::units::pressure()) ==
+            get_pow_of_dim(BD(), eagine::units::force()) -
+              get_pow_of_dim(BD(), eagine::units::area()),
+          "1");
+
+        test.check(
+          get_pow_of_dim(BD(), eagine::units::pressure()) ==
+            get_pow_of_dim(BD(), eagine::units::velocity()) +
+              get_pow_of_dim(BD(), eagine::units::mass()) -
+              get_pow_of_dim(BD(), eagine::units::time()) -
+              get_pow_of_dim(BD(), eagine::units::area()),
+          "2");
+    });
+}
+//------------------------------------------------------------------------------
 // main
 //------------------------------------------------------------------------------
 auto main(int argc, const char** argv) -> int {
-    eagitest::suite test{argc, argv, "dimension", 6};
+    eagitest::suite test{argc, argv, "dimension", 11};
     test.once(dimension_area);
     test.once(dimension_volume);
     test.once(dimension_mass_density);
     test.once(dimension_velocity);
     test.once(dimension_angular_velocity);
     test.once(dimension_acceleration);
+    test.once(dimension_momentum);
+    test.once(dimension_force);
+    test.once(dimension_energy);
+    test.once(dimension_power);
+    test.once(dimension_pressure);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
