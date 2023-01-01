b55418ce8d7ec23c92875a333b5763c96345df0e
Matus Chochlik <chochlik@gmail.com>
2023-01-01 07:57:04 +0100

    Added some tests for mp_string

--- source/modules/eagine/types/metaprogramming_test.cpp
+++ source/modules/eagine/types/metaprogramming_test.cpp
@@ -14,8 +14,10 @@ struct type_B {};
 struct type_C {};
 struct type_D {};
 //------------------------------------------------------------------------------
+// list
+//------------------------------------------------------------------------------
 void mp_list_contains(auto& s) {
-    eagitest::case_ test{s, 1, "contains"};
+    eagitest::case_ test{s, 1, "list contains"};
 
     using namespace eagine;
     using lABC = mp_list<type_A, type_B, type_C>;
@@ -33,7 +35,7 @@ void mp_list_contains(auto& s) {
 }
 //------------------------------------------------------------------------------
 void mp_list_union(auto& s) {
-    eagitest::case_ test{s, 2, "union"};
+    eagitest::case_ test{s, 2, "list union"};
 
     using namespace eagine;
     using lABC = mp_list<type_A, type_B, type_C>;
@@ -47,10 +49,105 @@ void mp_list_union(auto& s) {
     test.check(not mp_contains_v<lBC, type_D>, "does not contain D");
 }
 //------------------------------------------------------------------------------
+// string
+//------------------------------------------------------------------------------
+void mp_string_empty(auto& s) {
+    eagitest::case_ test{s, 3, "string empty"};
+    using namespace eagine;
+
+    using str = mp_string<>;
+    test.check_equal(std::strlen(str::value), std::size_t(0), "strlen");
+    test.check_equal(mp_strlen<str>::value, std::size_t(0), "length");
+    test.check(std::strcmp(str::value, "") == 0, "content");
+}
+//------------------------------------------------------------------------------
+void mp_string_content(auto& s) {
+    eagitest::case_ test{s, 4, "string content"};
+    using namespace eagine;
+
+    using str = mp_string<'m', 'p', '_', 's', 't', 'r', 'i', 'n', 'g'>;
+    test.check_equal(
+      std::strlen(str::value), std::strlen("mp_string"), "strlen");
+    test.check_equal(mp_strlen<str>::value, std::strlen("mp_string"), "length");
+    test.check(std::strcmp(str::value, "mp_string") == 0, "content");
+}
+//------------------------------------------------------------------------------
+struct mp_blah {
+    static constexpr const char mp_str[] = "blah";
+};
+//------------------------------------------------------------------------------
+void mp_string_make(auto& s) {
+    eagitest::case_ test{s, 5, "string make"};
+    using namespace eagine;
+
+    using str = mp_make_string_t<mp_blah>;
+    test.check_equal(std::strlen(str::value), std::strlen("blah"), "strlen");
+    test.check_equal(mp_strlen<str>::value, std::strlen("blah"), "length");
+    test.check(std::strcmp(str::value, "blah") == 0, "content");
+}
+//------------------------------------------------------------------------------
+void mp_string_concat(auto& s) {
+    eagitest::case_ test{s, 6, "string concatenate"};
+    using namespace eagine;
+
+    using foo = mp_string<'f', 'o', 'o'>;
+    using bar = mp_string<'b', 'a', 'r'>;
+    using baz = mp_string<'b', 'a', 'z'>;
+
+    using foobarbaz = mp_concat_t<foo, bar, baz>;
+    using bazbarfoo = mp_concat_t<baz, bar, foo>;
+
+    test.check_equal(
+      std::strlen(foobarbaz::value), std::strlen("foobarbaz"), "strlen");
+    test.check_equal(
+      mp_strlen<bazbarfoo>::value, std::strlen("bazbarfoo"), "length");
+    test.check(std::strcmp(foobarbaz::value, "foobarbaz") == 0, "content");
+    test.check(std::strcmp(bazbarfoo::value, "bazbarfoo") == 0, "content");
+}
+//------------------------------------------------------------------------------
+template <char C>
+struct test_lshift : eagine::mp_string<C - 1> {};
+//------------------------------------------------------------------------------
+void mp_string_translate(auto& s) {
+    eagitest::case_ test{s, 7, "string translate"};
+    using namespace eagine;
+
+    using bcdefg = mp_string<'b', 'c', 'd', 'e', 'f', 'g'>;
+
+    using str = mp_translate_t<bcdefg, test_lshift>;
+
+    test.check_equal(std::strlen(str::value), std::strlen("abcdef"), "strlen");
+    test.check_equal(mp_strlen<str>::value, std::strlen("abcdef"), "length");
+    test.check(std::strcmp(str::value, "abcdef") == 0, "content");
+}
+//------------------------------------------------------------------------------
+void mp_string_int_to_string(auto& s) {
+    eagitest::case_ test{s, 8, "string int to string"};
+    using namespace eagine;
+
+    using str1 = mp_int_to_string_t<-1234567890>;
+    using str2 = mp_int_to_string_t<123456789>;
+
+    test.check_equal(std::strlen(str1::value), std::size_t(11), "strlen");
+    test.check_equal(mp_strlen<str1>::value, std::size_t(11), "length");
+    test.check(std::strcmp(str1::value, "-1234567890") == 0, "content");
+    test.check_equal(std::strlen(str2::value), std::size_t(9), "strlen");
+    test.check_equal(mp_strlen<str2>::value, std::size_t(9), "length");
+    test.check(std::strcmp(str2::value, "123456789") == 0, "content");
+}
+//------------------------------------------------------------------------------
+// main
+//------------------------------------------------------------------------------
 auto main(int argc, const char** argv) -> int {
-    eagitest::suite test{argc, argv, "metaprogramming", 2};
+    eagitest::suite test{argc, argv, "metaprogramming", 8};
     test.once(mp_list_contains);
     test.once(mp_list_union);
+    test.once(mp_string_empty);
+    test.once(mp_string_content);
+    test.once(mp_string_make);
+    test.once(mp_string_concat);
+    test.once(mp_string_translate);
+    test.once(mp_string_int_to_string);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
