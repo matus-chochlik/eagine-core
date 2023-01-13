6535de47267738e6f1e734f435696ff1cd2fbe1c
Matus Chochlik <chochlik@gmail.com>
2023-01-13 04:50:18 +0100

    Merged some partitions of core.memory module

--- source/modules/eagine/memory/string_span.cpp
+++ source/modules/eagine/memory/string_span.cpp
@@ -10,9 +10,12 @@ export module eagine.core.memory:string_span;
 import eagine.core.types;
 import eagine.core.concepts;
 import :span;
+import <array>;
+import <cstdint>;
 import <cstring>;
 import <string>;
 import <string_view>;
+import <utility>;
 
 namespace eagine {
 namespace memory {
@@ -172,7 +175,31 @@ constexpr auto append_to(
     return str;
 }
 //------------------------------------------------------------------------------
+/// @brief Structural type wrapper for string literals.
+/// @ingroup string_utils
+/// @see string_view
+export template <std::size_t N>
+struct string_literal : std::array<const char, N> {
+
+    template <std::size_t... I>
+    constexpr string_literal(
+      const char (&s)[N],
+      std::index_sequence<I...>) noexcept
+      : std::array<const char, N>{{s[I]...}} {}
+
+    /// @brief Construction from a string literal (or char array).
+    constexpr string_literal(const char (&s)[N]) noexcept
+      : string_literal{s, std::make_index_sequence<N>()} {}
+
+    /// @brief Conversion to string view.
+    constexpr operator string_view() const noexcept {
+        return string_view{this->data(), span_size(this->size()) - 1};
+    }
+};
+//------------------------------------------------------------------------------
 } // namespace memory
+//------------------------------------------------------------------------------
+using memory::string_literal;
 //------------------------------------------------------------------------------
 // are_equal helper
 //------------------------------------------------------------------------------
