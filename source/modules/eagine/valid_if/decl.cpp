18ef76b4f4d4b752867011901af3dfbb3e43c097
Matus Chochlik <chochlik@gmail.com>
2023-02-02 09:02:18 +0100

    Merged some module fragments in valid_if

--- source/modules/eagine/valid_if/decl.cpp
+++ source/modules/eagine/valid_if/decl.cpp
@@ -841,4 +841,56 @@ struct within_limits<basic_valid_if<Dst, P, L, A...>, Src> {
     }
 };
 //------------------------------------------------------------------------------
+/// @brief Policy for always valid values.
+/// @ingroup valid_if
+export struct always_valid_policy {
+
+    /// @brief Indicates value validity. Always returns true.
+    template <typename T>
+    constexpr auto operator()(const T&) const noexcept {
+        return true;
+    }
+
+    struct do_log {
+        template <does_not_hide<do_log> X>
+        constexpr do_log(X&&) noexcept {}
+
+        template <typename Log, typename T>
+        void operator()(Log&, const T&) const {}
+    };
+};
+
+/// @brief Specialization of valid_if, for always valid values.
+/// @ingroup valid_if
+/// @see never_valid
+export template <typename T>
+using always_valid = valid_if<T, always_valid_policy>;
+//------------------------------------------------------------------------------
+/// @brief Policy for never-valid values.
+/// @ingroup valid_if
+export struct never_valid_policy {
+
+    /// @brief Indicates value validity. Always returns false.
+    template <typename T>
+    constexpr auto operator()(const T&) const noexcept {
+        return false;
+    }
+
+    struct do_log {
+        template <does_not_hide<do_log> X>
+        constexpr do_log(X&&) noexcept {}
+
+        template <typename Log, typename T>
+        void operator()(Log& log, const T&) const {
+            log << "value is not valid";
+        }
+    };
+};
+
+/// @brief Specialization of valid_if, for never-valid values.
+/// @ingroup valid_if
+/// @see always_valid
+export template <typename T>
+using never_valid = valid_if<T, never_valid_policy>;
+//------------------------------------------------------------------------------
 } // namespace eagine
