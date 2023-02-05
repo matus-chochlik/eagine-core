b77437bd830c8c3b5eda105e5a007b4c2184b069
Matus Chochlik <chochlik@gmail.com>
2023-02-05 10:04:15 +0100

    Added test for timeout

--- source/modules/eagine/utility/timeout_test.cpp
+++ source/modules/eagine/utility/timeout_test.cpp
@@ -20,8 +20,8 @@ void timeout_1(unsigned, auto& s) {
     const auto time_dur{
       std::chrono::milliseconds{test.random().get_between(R(250), R(1500))}};
     const auto start{std::chrono::steady_clock::now()};
-    eagine::timeout t{time_dur};
-    while(not t.is_expired()) {
+    eagine::timeout to{time_dur};
+    while(not to.is_expired()) {
         std::this_thread::yield();
     }
     const auto dur{std::chrono::steady_clock::now() - start};
@@ -29,9 +29,29 @@ void timeout_1(unsigned, auto& s) {
     test.check(dur < time_dur * 2, "duration at most");
 }
 //------------------------------------------------------------------------------
+void timeout_reset(auto& s) {
+    eagitest::case_ test{s, 2, "reset"};
+
+    using R = std::chrono::milliseconds::rep;
+    eagine::timeout to;
+    for(unsigned r = 0; r < test.repeats(10); ++r) {
+        const auto time_dur{std::chrono::milliseconds{
+          test.random().get_between(R(250), R(1500))}};
+        const auto start{std::chrono::steady_clock::now()};
+        to.reset(time_dur);
+        while(not to.is_expired()) {
+            std::this_thread::yield();
+        }
+        const auto dur{std::chrono::steady_clock::now() - start};
+        test.check(dur >= time_dur, "duration at least");
+        test.check(dur < time_dur * 2, "duration at most");
+    }
+}
+//------------------------------------------------------------------------------
 auto main(int argc, const char** argv) -> int {
-    eagitest::suite test{argc, argv, "timeout", 1};
+    eagitest::suite test{argc, argv, "timeout", 2};
     test.repeat(10, timeout_1);
+    test.once(timeout_reset);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
