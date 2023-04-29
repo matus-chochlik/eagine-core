82b589a6de23a2f79afb35a138038ebfe3e67bc8
Matus Chochlik <chochlik@gmail.com>
2023-04-29 16:31:18 +0200

    Additions to placeholder_expression

--- source/modules/eagine/types/placeholder.cpp
+++ source/modules/eagine/types/placeholder.cpp
@@ -8,39 +8,124 @@
 export module eagine.core.types:placeholder;
 
 import std;
+import :tribool;
+import :optional_reference;
 
 namespace eagine {
+//------------------------------------------------------------------------------
+export template <typename T>
+struct placeholder_terminal {
+    T value;
+
+    constexpr auto operator()(auto&&...) const noexcept -> const T& {
+        return value;
+    }
+};
+//------------------------------------------------------------------------------
+export template <typename T>
+consteval auto adapt_placeholder_arg(T&& value)
+  -> placeholder_terminal<std::remove_cvref_t<T>> {
+    return {std::forward<T>(value)};
+}
 //------------------------------------------------------------------------------
 export template <typename F>
 struct placeholder_expression {
     F f;
 
-    template <typename... Args>
-    constexpr auto operator()(Args&&... args) noexcept(noexcept(
-      std::declval<F>()(std::forward<Args>(args)...))) -> decltype(auto) {
-        return f(std::forward<Args>(args)...);
+    constexpr auto operator()(auto&&... args) noexcept(
+      noexcept(std::declval<F>()(decltype(args)(args)...))) -> decltype(auto) {
+        return f(decltype(args)(args)...);
     }
 
-    template <typename... Args>
-    constexpr auto operator()(Args&&... args) const
-      noexcept(noexcept(std::declval<const F>()(std::forward<Args>(args)...)))
+    constexpr auto operator()(auto&&... args) const
+      noexcept(noexcept(std::declval<const F>()(decltype(args)(args)...)))
         -> decltype(auto) {
-        return f(std::forward<Args>(args)...);
+        return f(decltype(args)(args)...);
     }
 };
+
+template <typename F>
+consteval auto make_placeholder_expression(F&& f) noexcept
+  -> placeholder_expression<std::remove_cvref_t<F>> {
+    return {f};
+}
 
 export template <typename F>
 placeholder_expression(F) -> placeholder_expression<F>;
 
+export template <typename F>
+consteval auto adapt_placeholder_arg(placeholder_expression<F> expr)
+  -> placeholder_expression<F> {
+    return expr;
+}
+//------------------------------------------------------------------------------
 export template <>
 struct placeholder_expression<std::integral_constant<size_t, 1>> {
-    template <typename Arg1, typename... Args>
-    constexpr auto operator()(Arg1&& arg1, Args&...) const noexcept
+    consteval auto member(auto ptr) const
+        requires(std::is_member_object_pointer_v<decltype(ptr)>)
+    {
+        return make_placeholder_expression(
+          [ptr](auto&& self, auto&&...) { return decltype(self)(self).*ptr; });
+    }
+
+    consteval auto optional(auto ptr) const
+        requires(std::is_member_object_pointer_v<decltype(ptr)>)
+    {
+        return make_placeholder_expression([ptr](auto&& self, auto&&...) {
+            return optional_reference(decltype(self)(self).*ptr);
+        });
+    }
+
+    consteval auto member(auto ptr, auto&&... args) const
+        requires(std::is_member_function_pointer_v<decltype(ptr)>)
+    {
+        return make_placeholder_expression(
+          [ptr, ... exprs{adapt_placeholder_arg(decltype(args)(args))}](
+            auto&& self, const auto&... as) {
+              return (decltype(self)(self).*ptr)(exprs(as...)...);
+          });
+    }
+
+    consteval auto optional(auto ptr, auto&&... args) const
+        requires(std::is_member_function_pointer_v<decltype(ptr)>)
+    {
+        return make_placeholder_expression(
+          [ptr, ... exprs{adapt_placeholder_arg(decltype(args)(args))}](
+            auto&& self, const auto&... as) {
+              using R = decltype((decltype(self)(self).*ptr)(exprs(as...)...));
+              if constexpr(std::is_pointer_v<R>) {
+                  return optional_reference<std::remove_pointer_t<R>>{
+                    (decltype(self)(self).*ptr)(exprs(as...)...)};
+              } else if constexpr(std::is_reference_v<R>) {
+                  return optional_reference<std::remove_reference_t<R>>{
+                    (decltype(self)(self).*ptr)(exprs(as...)...)};
+              } else {
+                  return std::optional<R>{
+                    (decltype(self)(self).*ptr)(exprs(as...)...)};
+              }
+          });
+    }
+
+    constexpr auto operator()(auto&& arg1, auto&&...) const noexcept
       -> decltype(auto) {
-        return std::forward<Arg1>(arg1);
+        return decltype(arg1)(arg1);
     }
 };
 export constinit const placeholder_expression<std::integral_constant<size_t, 1>>
   _1{};
+//------------------------------------------------------------------------------
+export template <typename X>
+consteval auto to_optional(placeholder_expression<X> e) noexcept {
+    return placeholder_expression{[e](auto&&... args) {
+        return std::optional{e(decltype(args)(args)...)};
+    }};
+}
+
+export template <typename X>
+consteval auto to_tribool(placeholder_expression<X> e) noexcept {
+    return placeholder_expression{[e](auto&&... args) {
+        return tribool{e(decltype(args)(args)...)};
+    }};
+}
 //------------------------------------------------------------------------------
 } // namespace eagine
