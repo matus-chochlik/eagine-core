2d0f3086e57904a0fc864d665e76e88b19b7356f
Matus Chochlik <chochlik@gmail.com>
2023-05-24 21:55:43 +0200

    Added the docker setup for the build-default container

---
+++ docker/build/default/toolchain-clang.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_BUILD_TYPE Debug)
+set(CMAKE_C_COMPILER clang-17)
+set(CMAKE_CXX_COMPILER clang++-17)
+set(CMAKE_CXX_FLAGS -stdlib=libc++)
+set(LLVM_PROFDATA llvm-profdata-17)
+set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++ -fuse-ld=lld")
+set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld")
+
