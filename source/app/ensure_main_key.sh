514d2ffc4d263ff9e7686413fe70b072c690ae66
Matus Chochlik <chochlik@gmail.com>
2023-06-22 08:00:38 +0200

    Additions and changes in build instructions for the .deb packages

--- source/app/ensure_main_key.sh
+++ source/app/ensure_main_key.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# coding=utf-8
+# Copyright Matus Chochlik.
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt
 #
-bits=${1:-2048}
-f="${HOME}/.config/eagine/defaults.yaml"
-mkdir -p "$(dirname ${f})" &&
-grep -qxF "main_key: " "${f}" ||\
-echo "main_key: $(dd if=/dev/random status=none bs=2048 count=1 | base64 -w 0)" >> "${f}"
+size=${1:-512}
+defaults="${HOME}/.config/eagine/defaults.yaml"
+mkdir -p "$(dirname ${defaults})" &&
+grep -qxF "main_key: " "${defaults}" ||\
+echo "main_key: $(dd if=/dev/random status=none bs=${size} count=1 | base64 -w 0)" >> "${defaults}"
 
