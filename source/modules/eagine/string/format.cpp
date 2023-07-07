0b3725a95edd62bbcfd22bbe49c66dd4732e9663
Matus Chochlik <chochlik@gmail.com>
2023-07-07 11:25:15 +0200

    Multiple changes in string format functions
    - Moved some function implementations into the impl file.
    - Added support for formatting into existing string.

--- source/modules/eagine/string/format.cpp
+++ source/modules/eagine/string/format.cpp
@@ -116,75 +116,14 @@ auto substitute_variable_into(
   string_view src,
   const string_view name,
   const string_view value,
-  const variable_substitution_options opts) noexcept -> std::string& {
-    do {
-        if(const auto found{find_position(src, name)}) {
-            const auto npos{*found};
-            if(
-              (npos >= 2) and (safe_add_lt(npos, name.size(), src.size())) and
-              (src[npos - 1] == opts.opening_bracket) and
-              (src[npos - 2] == opts.leading_sign) and
-              (src[npos + name.size()] == opts.closing_bracket)) {
-                append_to(head(src, npos - 2), dst);
-                append_to(value, dst);
-                src = skip(src, safe_add(npos, name.size(), 1));
-            } else {
-                append_to(head(src, safe_add(npos, name.size())), dst);
-                src = skip(src, safe_add(npos, name.size()));
-            }
-        } else {
-            append_to(src, dst);
-            src.reset();
-        }
-    } while(not src.empty());
-    return dst;
-}
+  const variable_substitution_options opts) noexcept -> std::string&;
 //------------------------------------------------------------------------------
 auto substitute_variables_into(
   std::string& dst,
   string_view src,
   const callable_ref<std::optional<string_view>(const string_view) noexcept>&
     translate,
-  const variable_substitution_options opts) noexcept -> std::string& {
-
-    do {
-        if(const auto lpos{find_element(src, opts.leading_sign)}) {
-            append_to(head(src, lpos.value()), dst);
-            src = skip(src, lpos.value());
-
-            if(const auto inner{slice_inside_brackets(
-                 src, opts.opening_bracket, opts.closing_bracket)}) {
-
-                if(const auto translation{translate(inner)}) {
-                    append_to(translation.value(), dst);
-                } else {
-                    if(find_element(inner, opts.leading_sign)) {
-                        std::string temp;
-                        substitute_variables_into(temp, inner, translate, opts);
-                        if(const auto translation2 = translate(temp)) {
-                            append_to(translation2.value(), dst);
-                        } else if(opts.keep_untranslated) {
-                            append_to(
-                              head(src, safe_add(inner.size(), 3)), dst);
-                        }
-                    } else if(opts.keep_untranslated) {
-                        append_to(head(src, safe_add(inner.size(), 3)), dst);
-                    }
-                }
-                src = skip(src, safe_add(inner.size(), 3));
-            } else {
-                append_to(head(src, 3), dst);
-                src = skip(src, 3);
-            }
-
-        } else {
-            append_to(src, dst);
-            src.reset();
-        }
-    } while(not src.empty());
-
-    return dst;
-}
+  const variable_substitution_options opts) noexcept -> std::string&;
 //------------------------------------------------------------------------------
 export [[nodiscard]] auto substitute_variables(
   const string_view src,
@@ -198,35 +137,18 @@ export [[nodiscard]] auto substitute_variables(
 export [[nodiscard]] auto substitute_variables(
   const std::string& str,
   const span<const std::string> strings,
-  const variable_substitution_options opts = {}) noexcept -> std::string {
-    const auto translate_func =
-      [&strings](string_view key) -> std::optional<string_view> {
-        char* e = nullptr; // NOLINT(hicpp-vararg)
-        const span_size_t idx = span_size(std::strtol(c_str(key), &e, 10));
-        if((0 < idx) and (idx <= strings.size())) {
-            return {{strings[idx - 1]}};
-        }
-        return {};
-    };
-    return substitute_variables(str, {construct_from, translate_func}, opts);
-}
+  const variable_substitution_options opts = {}) noexcept -> std::string;
+//------------------------------------------------------------------------------
+export [[nodiscard]] auto substitute_variables_into(
+  std::string& dest,
+  const string_view str,
+  const span<const std::string> strings,
+  const variable_substitution_options opts = {}) noexcept -> std::string&;
 //------------------------------------------------------------------------------
 export [[nodiscard]] auto substitute_variables(
   const std::string& str,
   const std::map<std::string, std::string, str_view_less>& dictionary,
-  const variable_substitution_options opts) noexcept -> std::string {
-    const auto translate_func =
-      [&dictionary](string_view key) -> std::optional<string_view> {
-        if(not dictionary.empty()) {
-            auto i = dictionary.find(key);
-            if(i != dictionary.end()) {
-                return {i->second};
-            }
-        }
-        return {};
-    };
-    return substitute_variables(str, {construct_from, translate_func}, opts);
-}
+  const variable_substitution_options opts) noexcept -> std::string;
 //------------------------------------------------------------------------------
 class format_string_and_list_base {
 
@@ -241,6 +163,11 @@ protected:
 
     auto _fmt(span<const std::string> values) const noexcept -> std::string {
         return substitute_variables(_fmt_str, values);
+    }
+
+    auto _fmt_into(std::string& dest, span<const std::string> values)
+      const noexcept -> std::string& {
+        return substitute_variables_into(dest, _fmt_str, values);
     }
 
 private:
@@ -258,6 +185,10 @@ public:
 
     [[nodiscard]] operator std::string() const noexcept {
         return _fmt({});
+    }
+
+    auto into(std::string& dest) const noexcept -> std::string& {
+        return _fmt_into(dest, {});
     }
 };
 //------------------------------------------------------------------------------
@@ -289,6 +220,11 @@ public:
         return _fmt(view(_list));
     }
 
+    /// @brief Formats the string with the variables substituted into a string.
+    auto into(std::string& dest) const noexcept -> std::string& {
+        return _fmt_into(dest, view(_list));
+    }
+
 public:
     std::array<std::string, N> _list{};
 };
@@ -305,6 +241,10 @@ public:
 
     [[nodiscard]] operator std::string() const noexcept {
         return _fmt(view(_list));
+    }
+
+    auto into(std::string& dest) const noexcept -> std::string& {
+        return _fmt_into(dest, view(_list));
     }
 
 public:
