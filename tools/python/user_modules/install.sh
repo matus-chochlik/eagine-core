8ff8167f9510cf77f09a4f12f876fadff7da3cff
Matus Chochlik <chochlik@gmail.com>
2023-07-16 10:22:54 +0200

    Added the python eagine.argparseUtils module

---
+++ tools/python/user_modules/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright Matus Chochlik.
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt
+#
+this_dir="$(realpath $(dirname ${0}))"
+user_site="$(python3 -m site --user-site)"
+if [[ ! -d "${user_site}" ]]
+then mkdir -p "${user_site}"
+fi
+
+cp -r -f "${this_dir}/eagine" "${user_site}"
