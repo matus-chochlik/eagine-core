21973c0d20913ecd9992fa45895355463ea0acf2
Matus Chochlik <chochlik@gmail.com>
2023-07-25 08:02:31 +0200

    Added first version of support for resource signatures to fmd_modify

--- source/app/fmd_modify.py
+++ source/app/fmd_modify.py
@@ -8,10 +8,103 @@ import os
 import re
 import sys
 import json
+import base64
 import shutil
 import argparse
 import tempfile
 
+# ------------------------------------------------------------------------------
+# Digital signatures
+# ------------------------------------------------------------------------------
+#  OpenSSL.crypto
+# ------------------------------------------------------------------------------
+class OpenSSLDataSigner(object):
+    # -------------------------------------------------------------------------
+    def __init__(self, arg_parser, crypto):
+        self._crypto = crypto
+        self._cert = None
+        self._pkey = None
+
+        arg_parser.add_argument(
+            "--sign-certificate",
+            metavar="FILE",
+            nargs='?',
+            type=os.path.realpath,
+            default=None
+        )
+        arg_parser.add_argument(
+            "--sign-key",
+            metavar="FILE",
+            nargs='?',
+            type=os.path.realpath,
+            default=None
+        )
+        arg_parser.add_argument(
+            "--sign-hash",
+            metavar="HASH-ID",
+            nargs='?',
+            choices=["sha256", "sha384", "sha512"],
+            default="sha256"
+        )
+    # -------------------------------------------------------------------------
+    def processParsedOptions(self, arg_parser, options):
+        pass
+
+    # -------------------------------------------------------------------------
+    def beginSign(self, options):
+        with open(options.sign_certificate, "r") as certfd:
+            self._cert = self._crypto.load_certificate(
+                self._crypto.FILETYPE_PEM,
+                certfd.read());
+        with open(options.sign_key, "r") as pkeyfd:
+            self._pkey = self._crypto.load_privatekey(
+                self._crypto.FILETYPE_PEM,
+                pkeyfd.read());
+        self._data = bytes()
+
+    # -------------------------------------------------------------------------
+    def addData(self, options, data):
+        if isinstance(data, str):
+            data = data.encode("utf-8")
+        assert isinstance(data, bytes)
+        self._data += data
+
+    # -------------------------------------------------------------------------
+    def signData(self, options):
+        signature = base64.b64encode(
+            self._crypto.sign(self._pkey, self._data, options.sign_hash))
+        signature = signature.decode("ascii")
+        return signature
+
+    # -------------------------------------------------------------------------
+    def verifySignature(self, options, signature):
+        sig = signature.encode("ascii")
+        sig = base64.b64decode(sig)
+        self._crypto.verify(self._cert, sig, self._data, options.sign_hash)
+        return True
+
+    # -------------------------------------------------------------------------
+    def finishSign(self, options):
+        sig = {}
+        signature = self.signData(options)
+        if self.verifySignature(options, signature):
+            with open(options.sign_certificate, "rt") as certfd:
+                sig["certificate"] = certfd.read();
+            sig["signature"] = signature
+        self._data = None
+        self._cert = None
+        self._pkey = None
+        return sig
+
+# ------------------------------------------------------------------------------
+def makeSigner(arg_parser):
+    # TODO pyca/cryptography
+    try:
+        from OpenSSL import crypto
+        return OpenSSLDataSigner(arg_parser, crypto)
+    except ImportError:
+        pass
+    return None
 # ------------------------------------------------------------------------------
 # Argument parsing
 # ------------------------------------------------------------------------------
@@ -45,7 +138,7 @@ class ArgumentParser(argparse.ArgumentParser):
                             return True
             return x
 
-        def _valid_deletion(x):
+        def _valid_attribute(x):
             try:
                 assert self._path_re.match(x)
                 return x
@@ -86,7 +179,7 @@ class ArgumentParser(argparse.ArgumentParser):
             metavar='ATTRIBUTE',
             dest='deletions',
             nargs='?',
-            type=_valid_deletion,
+            type=_valid_attribute,
             action="append",
             default=[]
         )
@@ -108,6 +201,18 @@ class ArgumentParser(argparse.ArgumentParser):
             type=_valid_input_path
         )
 
+        self._signer = makeSigner(self)
+        if self._signer is not None:
+            self.add_argument(
+                "--sign-attribute",
+                metavar='ATTRIBUTE',
+                dest='sign_attributes',
+                nargs='?',
+                type=_valid_attribute,
+                action="append",
+                default=[]
+            )
+
     # -------------------------------------------------------------------------
     def processParsedOptions(self, options):
         options.input_paths = set(options.input_paths)
@@ -125,6 +230,12 @@ class ArgumentParser(argparse.ArgumentParser):
         options.additions = [(_processKey(k), v) for k, v in options.additions]
         options.updates = [(_processKey(k), v) for k, v in options.updates]
         options.deletions = [_processKey(k) for k in options.deletions]
+
+        options.signer = self._signer
+        if options.signer is not None:
+            options.sign_attributes = [_processKey(k) for k in options.sign_attributes]
+            options.signer.processParsedOptions(self, options)
+
         return options
     # -------------------------------------------------------------------------
     def parseArgs(self):
@@ -181,44 +292,83 @@ def formatHeader(options, header, ofd):
             _write(",")
         _write(json.dumps(key))
         _write(':')
-        if isinstance(value, dict):
-            _write(json.dumps(value))
-        else:
-            _write(json.dumps(value))
+        _write(json.dumps(value, separators=(',',':')))
         _write('\n')
     _write("}")
 # ------------------------------------------------------------------------------
+def getSignedAttributes(options, header):
+    signed = {}
+    for attrib in options.sign_attributes:
+        src = header
+        dst = signed
+        for key in attrib[:-1]:
+            src = src.setdefault(key, {})
+            dst = dst.setdefault(key, {})
+        dst[attrib[-1]] = src[attrib[-1]]
+    return json.dumps(
+        signed,
+        sort_keys=True,
+        ensure_ascii=True,
+        separators=(',',':'))
+# ------------------------------------------------------------------------------
+def addSignature(signer, options, header, datafd):
+    signer.beginSign(options)
+    signer.addData(options, getSignedAttributes(options, header))
+    while True:
+        chunk = datafd.read(4096)
+        if not chunk:
+            break
+        signer.addData(options, chunk)
+    sig = signer.finishSign(options)
+    if sig is not None:
+        sig["attributes"] = ['.'.join(a) for a in options.sign_attributes]
+        header.setdefault("signed", []).append(sig)
+# ------------------------------------------------------------------------------
 def processFile(options, path):
-    with open(path, "rb") as ifd:
+    with open(path, "rb") as inputfd:
         decoder = json.JSONDecoder()
         head_bytes = bytes()
-        while True:
-            chunk = ifd.read(1024)
-            if not chunk:
-                break
-            head_bytes += chunk
-            try:
-                data = bytes()
-                head_str = head_bytes.decode("utf-8")
-            except UnicodeDecodeError as ude:
-                data = head_bytes[ude.start:]
-                head_bytes = head_bytes[:ude.start]
-                head_str = head_bytes.decode("utf-8")
-            try:
-                header, pos = decoder.raw_decode(head_str)
-                data = head_str[pos:].encode("utf-8") + data
-                break
-            except json.JSONDecodeError as jde:
-                pass
-        with tempfile.NamedTemporaryFile(mode='wb', delete=False) as ofd:
-            formatHeader(options, updateHeader(options, header), ofd)
-            ofd.write(data)
+        with tempfile.NamedTemporaryFile(mode='wb', delete=False) as datafd:
             while True:
-                chunk = ifd.read(4096)
+                chunk = inputfd.read(1024)
                 if not chunk:
                     break
-                ofd.write(chunk)
-        shutil.move(ofd.name, path)
+                head_bytes += chunk
+                try:
+                    data = bytes()
+                    head_str = head_bytes.decode("utf-8")
+                except UnicodeDecodeError as ude:
+                    data = head_bytes[ude.start:]
+                    head_bytes = head_bytes[:ude.start]
+                    head_str = head_bytes.decode("utf-8")
+                try:
+                    header, pos = decoder.raw_decode(head_str)
+                    data = head_str[pos:].encode("utf-8") + data
+                    break
+                except json.JSONDecodeError as jde:
+                    pass
+            datafd.write(data)
+            while True:
+                chunk = inputfd.read(4096)
+                if not chunk:
+                    break
+                datafd.write(chunk)
+
+        header = updateHeader(options, header)
+        if options.signer is not None:
+            with open(datafd.name, "rb") as datafd:
+                addSignature(options.signer, options, header, datafd)
+
+        with open(datafd.name, "rb") as datafb:
+            with tempfile.NamedTemporaryFile(mode='wb', delete=False) as outputfd:
+                formatHeader(options, header, outputfd)
+                while True:
+                    chunk = datafb.read(4096)
+                    if not chunk:
+                        break
+                    outputfd.write(chunk)
+            shutil.move(outputfd.name, path)
+        os.unlink(datafd.name)
 
 # ------------------------------------------------------------------------------
 def processFiles(options):
