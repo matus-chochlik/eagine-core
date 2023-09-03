64c3b445348b0c46bf0dd499784d05c2e0cfc81f
Matus Chochlik <chochlik@gmail.com>
2023-09-03 09:24:41 +0200

    Added the (bytes|hex)_to_(rgba) functions

--- source/modules/eagine/math/colors.cpp
+++ source/modules/eagine/math/colors.cpp
@@ -8,6 +8,7 @@
 export module eagine.core.math:colors;
 
 import std;
+import eagine.core.types;
 import :constants;
 import :functions;
 import :vector;
@@ -78,6 +79,78 @@ export template <std::floating_point T, bool V>
         return {x + m, z + m, c + m};
     }
     return {c + m, z + m, x + m};
+}
+//------------------------------------------------------------------------------
+export template <std::floating_point T = float, bool V = true>
+[[nodiscard]] constexpr auto bytes_to_rgb(byte r, byte g, byte b) noexcept
+  -> vector<T, 3, V> {
+    return {
+      T(r) / T(std::numeric_limits<byte>::max()),
+      T(g) / T(std::numeric_limits<byte>::max()),
+      T(b) / T(std::numeric_limits<byte>::max())};
+}
+//------------------------------------------------------------------------------
+export template <std::floating_point T = float, bool V = true>
+[[nodiscard]] constexpr auto bytes_to_rgb(byte r, byte g, byte b, byte a) noexcept
+  -> vector<T, 4, V> {
+    return {
+      T(r) / T(std::numeric_limits<byte>::max()),
+      T(g) / T(std::numeric_limits<byte>::max()),
+      T(b) / T(std::numeric_limits<byte>::max()),
+      T(a) / T(std::numeric_limits<byte>::max())};
+}
+//------------------------------------------------------------------------------
+export template <std::floating_point T = float, bool V = true>
+[[nodiscard]] constexpr auto bytes_to_rgb(std::uint32_t c) noexcept
+  -> vector<T, 3, V> {
+    return bytes_to_rgb<T, V>(
+      (c >> 16U) & std::numeric_limits<byte>::max(),
+      (c >> 8U) & std::numeric_limits<byte>::max(),
+      (c >> 0U) & std::numeric_limits<byte>::max());
+}
+//------------------------------------------------------------------------------
+export template <std::floating_point T = float, bool V = true>
+[[nodiscard]] constexpr auto bytes_to_rgb(std::uint32_t c) noexcept
+  -> vector<T, 4, V> {
+    return bytes_to_rgb<T, V>(
+      (c >> 24U) & std::numeric_limits<byte>::max(),
+      (c >> 16U) & std::numeric_limits<byte>::max(),
+      (c >> 8U) & std::numeric_limits<byte>::max(),
+      (c >> 0U) & std::numeric_limits<byte>::max());
+}
+//------------------------------------------------------------------------------
+static constexpr auto chars2byte(const char h, const char l) noexcept {
+    const auto char2byte{[](const char c) {
+        if(('0' <= c) and (c <= '9')) {
+            return byte(c - '0');
+        } else if(('a' <= c) and (c <= 'f')) {
+            return byte(10U + (c - 'a'));
+        } else if(('A' <= c) and (c <= 'F')) {
+            return byte(10U + (c - 'A'));
+        } else {
+            return byte{0U};
+        }
+    }};
+    return byte((char2byte(h) << 4U) | char2byte(l));
+}
+//------------------------------------------------------------------------------
+export template <std::floating_point T = float, bool V = true>
+[[nodiscard]] constexpr auto hex_to_rgb(const char (&hex)[8]) noexcept
+  -> vector<T, 3, V> {
+    return bytes_to_rgb<T, V>(
+      chars2byte(hex[1], hex[2]),
+      chars2byte(hex[3], hex[4]),
+      chars2byte(hex[5], hex[6]));
+}
+//------------------------------------------------------------------------------
+export template <std::floating_point T = float, bool V = true>
+[[nodiscard]] constexpr auto hex_to_rgba(const char (&hex)[10]) noexcept
+  -> vector<T, 3, V> {
+    return bytes_to_rgb<T, V>(
+      chars2byte(hex[1], hex[2]),
+      chars2byte(hex[3], hex[4]),
+      chars2byte(hex[5], hex[6]),
+      chars2byte(hex[7], hex[8]));
 }
 //------------------------------------------------------------------------------
 } // namespace eagine::math
