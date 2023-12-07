e9904767645a2b85cdf95e3168b62e5f076ebebb
Matus Chochlik <chochlik@gmail.com>
2023-12-07 07:57:29 +0100

    Small changes in various scripts
    - Fixed paths in eagilog-psql.
    - Fixes in the eagilog docker setup helper scripts.

--- source/app/log-psql.sh
+++ source/app/log-psql.sh
@@ -4,7 +4,7 @@
 # See accompanying file LICENSE_1_0.txt or copy at
 #  http://www.boost.org/LICENSE_1_0.txt
 #
-PGPASSWORD=$(< "${HOME}/.config/eagine/eagilog/psql/secrets/eagilog_pg.passwd") \
+PGPASSWORD=$(< "${HOME}/.config/eagine/eagilog/secrets/eagilog_pg.passwd") \
 psql \
 	-h localhost \
 	-d eagilog \
