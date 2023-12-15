7c0add4692b27d79c1ac49dc8571a33c8007eda2
Matus Chochlik <chochlik@gmail.com>
2023-12-15 04:28:16 +0100

    Additions to the eagilog-web server

---
+++ docker/logging/server/web/static/active_stream_main_progress.js
@@ -0,0 +1,23 @@
+var isActive = false;
+var chartReloadCount = 0;
+
+doReload = function() {
+    chartReloadCount += 1;
+    if (isActive || chartReloadCount >= 10) {
+        $('#active_stream_main_progress')
+            .attr('src', '/image/active_stream_main_progress.svg?' + new Date().getTime())
+        chartReloadCount = 0;
+    }
+};
+
+$(window).focus(function() {
+    isActive = true;
+    doReload();
+});
+
+$(window).blur(function() {
+    isActive = false;
+    chartReloadCount = 0;
+});
+
+setInterval(doReload, 15000);
