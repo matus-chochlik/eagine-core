f9d14fcb107fe93ec28ee71ff3b07228f60a16b2
Matus Chochlik <chochlik@gmail.com>
2023-12-21 06:43:35 +0100

    Updated tests for double_buffer

--- source/modules/eagine/utility/double_buffer_test.cpp
+++ source/modules/eagine/utility/double_buffer_test.cpp
@@ -15,13 +15,13 @@ void double_buffer_1(auto& s) {
     eagine::double_buffer<int> db{1};
     const auto& cdb{db};
 
-    test.check_equal(cdb.front(), 1, "front 1 A");
-    test.check_equal(cdb.back(), 1, "back 1 A");
+    test.check_equal(cdb.current(), 1, "current 1 A");
+    test.check_equal(cdb.next(), 1, "next 1 A");
     db.swap();
-    test.check_equal(db.front(), 1, "front 1 B");
-    test.check_equal(db.back(), 1, "back 1 B");
+    test.check_equal(db.current(), 1, "current 1 B");
+    test.check_equal(db.next(), 1, "next 1 B");
 
-    db.back() = 2;
+    db.next() = 2;
     test.check_equal(db.front(), 1, "front 1 C");
     test.check_equal(db.back(), 2, "back 2 C");
     db.swap();
@@ -33,13 +33,42 @@ void double_buffer_1(auto& s) {
     test.check_equal(cdb.back(), 2, "back 2 E");
     db.front() = 3;
     db.swap();
-    test.check_equal(cdb.front(), 2, "front 2 F");
-    test.check_equal(cdb.back(), 3, "back 3 F");
+    test.check_equal(cdb.current(), 2, "front 2 F");
+    test.check_equal(cdb.next(), 3, "back 3 F");
+}
+//------------------------------------------------------------------------------
+void double_buffer_2(auto& s) {
+    eagitest::case_ test{s, 2, "2"};
+
+    eagine::double_buffer<int> db{1, 2};
+    const auto& cdb{db};
+
+    test.check_equal(cdb.current(), 1, "current 1 A");
+    test.check_equal(cdb.next(), 2, "next 2 A");
+    db.swap();
+    test.check_equal(db.current(), 2, "current 2 B");
+    test.check_equal(db.next(), 1, "next 1 B");
+
+    db.next() = 3;
+    test.check_equal(db.front(), 2, "front 2 C");
+    test.check_equal(db.back(), 3, "back 3 C");
+    db.swap();
+    test.check_equal(db.front(), 3, "front 3 D");
+    test.check_equal(db.back(), 2, "back 2 D");
+
+    db.back() = 4;
+    test.check_equal(cdb.front(), 3, "front 3 E");
+    test.check_equal(cdb.back(), 4, "back 4 E");
+    db.current() = 5;
+    db.swap();
+    test.check_equal(cdb.current(), 4, "front 4 F");
+    test.check_equal(cdb.next(), 5, "back 5 F");
 }
 //------------------------------------------------------------------------------
 auto main(int argc, const char** argv) -> int {
-    eagitest::suite test{argc, argv, "double buffer", 1};
+    eagitest::suite test{argc, argv, "double buffer", 2};
     test.once(double_buffer_1);
+    test.once(double_buffer_2);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
