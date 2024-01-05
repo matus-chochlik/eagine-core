30c8d7087bb8a5b417d687d2b50ae9eff72161cc
Matus Chochlik <chochlik@gmail.com>
2024-01-05 14:21:08 +0100

    Cleanup in special command-line argument handling

--- source/modules/eagine/app/special_args.cpp
+++ source/modules/eagine/app/special_args.cpp
@@ -14,14 +14,6 @@ import std;
 
 namespace eagine {
 //------------------------------------------------------------------------------
-export void print_version(main_ctx&, std::ostream&);
-//------------------------------------------------------------------------------
-export void print_copyright_notice(main_ctx&, std::ostream&);
-//------------------------------------------------------------------------------
-void do_print_resource(main_ctx&, std::ostream&, const embedded_resource& res);
-//------------------------------------------------------------------------------
-export void print_bash_completion(main_ctx&, std::ostream&);
-//------------------------------------------------------------------------------
 // common special command line arguments
 //------------------------------------------------------------------------------
 export auto handle_common_special_args(main_ctx& ctx) -> std::optional<int>;
--- source/modules/eagine/app/special_args_impl.cpp
+++ source/modules/eagine/app/special_args_impl.cpp
@@ -21,32 +21,38 @@ namespace eagine {
 //------------------------------------------------------------------------------
 // version
 //------------------------------------------------------------------------------
-void print_version(main_ctx&, std::ostream& out) {
-    out << config_git_version().value_or("unknown") << std::endl;
+void print_version(main_ctx&) {
+    std::cout << config_git_version().value_or("unknown") << std::endl;
+}
+//------------------------------------------------------------------------------
+void print_git_hash(main_ctx&) {
+    std::cout << config_git_hash_id().value_or("unknown") << std::endl;
 }
 //------------------------------------------------------------------------------
 // copyright / license
 //------------------------------------------------------------------------------
-void print_copyright_notice(main_ctx& ctx, std::ostream& out) {
+void print_copyright_notice(main_ctx& ctx) {
+    using std::cout;
+
     const auto print{[&](const memory::const_block data) {
-        write_to_stream(out, data);
+        write_to_stream(cout, data);
         return true;
     }};
 
-    out << "Copyright © 2015-2024 Matúš Chochlík.\n";
+    cout << "Copyright © 2015-2024 Matúš Chochlík.\n";
 
     if(const auto license_res{search_resource("LicenseTxt")}) {
-        out << "Distributed under the following license:\n";
-        out << '\n';
+        cout << "Distributed under the following license:\n";
+        cout << '\n';
         license_res.fetch(ctx, {construct_from, print});
     } else if(const auto boost_license_res{search_resource("BoostLcnse")}) {
-        out << "Distributed under the Boost Software License, Version 1.0.\n";
-        out << '\n';
+        cout << "Distributed under the Boost Software License, Version 1.0.\n";
+        cout << '\n';
         boost_license_res.fetch(ctx, {construct_from, print});
     } else {
-        out << "Distributed under the Boost Software License, Version 1.0.\n";
-        out << "See accompanying file LICENSE_1_0.txt or copy at\n";
-        out << " http://www.boost.org/LICENSE_1_0.txt\n";
+        cout << "Distributed under the Boost Software License, Version 1.0.\n";
+        cout << "See accompanying file LICENSE_1_0.txt or copy at\n";
+        cout << " http://www.boost.org/LICENSE_1_0.txt\n";
     }
 }
 //------------------------------------------------------------------------------
@@ -66,7 +72,11 @@ void print_resource(main_ctx& ctx, const embedded_resource& res) {
 //------------------------------------------------------------------------------
 auto handle_common_special_args(main_ctx& ctx) -> std::optional<int> {
     if(ctx.args().find("--version")) {
-        print_version(ctx, std::cout);
+        print_version(ctx);
+        return {0};
+    }
+    if(ctx.args().find("--git-hash")) {
+        print_git_hash(ctx);
         return {0};
     }
     if(const auto arg{ctx.args().find("--version-at-least")}) {
@@ -78,7 +88,7 @@ auto handle_common_special_args(main_ctx& ctx) -> std::optional<int> {
         return {2};
     }
     if(ctx.args().find("--copyright") or ctx.args().find("--license")) {
-        print_copyright_notice(ctx, std::cout);
+        print_copyright_notice(ctx);
         return {0};
     }
     if(ctx.args().find("--print-bash-completion")) {
