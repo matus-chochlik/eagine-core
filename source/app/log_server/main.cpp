cb56be23832a4943574a2f89f20187c5d80c33ce
Matus Chochlik <chochlik@gmail.com>
2024-01-05 06:11:25 +0100

    Added the --print-psql-schema option to eagine-core-log-server

--- source/app/log_server/main.cpp
+++ source/app/log_server/main.cpp
@@ -13,9 +13,41 @@ import eagine.core.log_server;
 
 namespace eagine {
 //------------------------------------------------------------------------------
+// print text from resource
+//------------------------------------------------------------------------------
+void do_print_resource(
+  main_ctx& ctx,
+  std::ostream& out,
+  const embedded_resource& res) {
+    if(res) {
+        const auto print{[&](const memory::const_block data) {
+            write_to_stream(out, data);
+            return true;
+        }};
+        res.fetch(ctx, {construct_from, print});
+    }
+}
+//------------------------------------------------------------------------------
+template <std::size_t L>
+void print_resource(main_ctx& ctx, std::ostream& out, const char (&res_id)[L]) {
+    do_print_resource(ctx, out, search_resource(res_id));
+}
+//------------------------------------------------------------------------------
+// special args handling
+//------------------------------------------------------------------------------
+auto handle_special_args(main_ctx& ctx) -> std::optional<int> {
+    if(ctx.args().find("--print-psql-schema")) {
+        print_resource(ctx, std::cout, "PSQLSchema");
+        return {0};
+    }
+    return handle_common_special_args(ctx);
+}
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
 auto main(main_ctx& ctx) -> int {
     try {
-        if(const auto exit_code{handle_common_special_args(ctx)}) {
+        if(const auto exit_code{handle_special_args(ctx)}) {
             return *exit_code;
         }
         if(const auto sinks{logs::make_sink_factory(ctx)}) {
