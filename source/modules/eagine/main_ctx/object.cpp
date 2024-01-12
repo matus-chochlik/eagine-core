c005bfa3f020e1c8aa690a8a30b25454f66de45d
Matus Chochlik <chochlik@gmail.com>
2024-01-12 06:30:25 +0100

    Added new constructors to main_ctx_buffer

--- source/modules/eagine/main_ctx/object.cpp
+++ source/modules/eagine/main_ctx/object.cpp
@@ -35,6 +35,14 @@ export class main_ctx_buffer : public memory::buffer {
 public:
     main_ctx_buffer(main_ctx_object& parent) noexcept
       : _parent{parent} {}
+    main_ctx_buffer(main_ctx_object& parent, memory::buffer buf) noexcept
+      : memory::buffer{std::move(buf)}
+      , _parent{parent} {}
+    main_ctx_buffer(main_ctx_object& parent, span_size_t size) noexcept
+      : _parent{parent} {
+        get(size);
+    }
+
     main_ctx_buffer(main_ctx_buffer&&) = delete;
     main_ctx_buffer(const main_ctx_buffer&) = delete;
     auto operator=(main_ctx_buffer&&) = delete;
