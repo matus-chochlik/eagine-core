43190aa61be96f938e7f239aa6086db2958c3ee9
Matus Chochlik <chochlik@gmail.com>
2024-01-27 21:20:19 +0100

    Removed obsolete commands from the script building core-dev deb package

--- deploy/dpkg/dev/module-std-opts.cmake
+++
@@ -1,8 +0,0 @@
-# Copyright Matus Chochlik.
-# Distributed under the Boost Software License, Version 1.0.
-# See accompanying file LICENSE_1_0.txt or copy at
-#  http://www.boost.org/LICENSE_1_0.txt
-#
-target_compile_options(
-	std
-	INTERFACE -Wno-reserved-module-identifier)
