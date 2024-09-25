4c980709c837251cbadfe35611a49769135a2fc5
Matus Chochlik <chochlik@gmail.com>
2024-09-25 05:23:39 +0200

    Extended the simd_ident example

--- example/eagine/simd_ident.cpp
+++ example/eagine/simd_ident.cpp
@@ -10,20 +10,17 @@ import std;
 
 namespace eagine {
 //------------------------------------------------------------------------------
-template <bool useSimd>
-auto ident_ops(auto& out, std::integral_constant<bool, useSimd> = {}) noexcept {
-    using ident_t =
-      basic_identifier<10, 6, default_identifier_char_set, identifier_t, useSimd>;
-
-    ident_t id{};
-    std::vector<ident_t> vid;
+template <typename Ident, bool useSimd>
+auto ident_ops_t(auto& out, std::integral_constant<bool, useSimd>) noexcept {
+    Ident id{};
+    std::vector<Ident> vid;
 
     const std::size_t n = 128Z * 1024Z;
     vid.reserve(n);
 
     const time_measure measurement;
 
-    const auto sort_pred{[](const ident_t l, const ident_t r) {
+    const auto sort_pred{[](const Ident l, const Ident r) {
         return r < l;
     }};
 
@@ -45,13 +42,41 @@ auto ident_ops(auto& out, std::integral_constant<bool, useSimd> = {}) noexcept {
     return seconds;
 }
 //------------------------------------------------------------------------------
+template <bool useSimd>
+auto ident_ops(
+  auto& out,
+  std::integral_constant<bool, useSimd> use_simd = {}) noexcept {
+    return ident_ops_t<
+      basic_identifier<10, 6, default_identifier_char_set, identifier_t, useSimd>>(
+      out, use_simd);
+}
+//------------------------------------------------------------------------------
+template <bool useSimd>
+auto id_v_ops(
+  auto& out,
+  std::integral_constant<bool, useSimd> use_simd = {}) noexcept {
+    return ident_ops_t<basic_identifier_value<
+      10,
+      6,
+      default_identifier_char_set,
+      identifier_t,
+      useSimd>>(out, use_simd);
+}
+//------------------------------------------------------------------------------
 auto main(main_ctx& ctx) -> int {
-    main_ctx_object out{"matrix", ctx};
+    main_ctx_object out{"identifier", ctx};
 
-    const auto t_simd{ident_ops(out, std::true_type{})};
-    const auto t_base{ident_ops(out, std::false_type{})};
+    const auto t_simd_i{ident_ops(out, std::true_type{})};
+    const auto t_base_i{ident_ops(out, std::false_type{})};
 
-    out.cio_print("speedup: ${val}").arg("val", t_simd / t_base);
+    out.cio_print("speedup (identifier): ${val}")
+      .arg("val", t_simd_i / t_base_i);
+
+    const auto t_simd_v{id_v_ops(out, std::true_type{})};
+    const auto t_base_v{id_v_ops(out, std::false_type{})};
+
+    out.cio_print("speedup (identifier_value): ${val}")
+      .arg("val", t_simd_v / t_base_v);
 
     return 0;
 }
